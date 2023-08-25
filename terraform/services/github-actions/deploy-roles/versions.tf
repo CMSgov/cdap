068b87fb708024b53275e5f158a0780193efa33b
HK <hk@ip-10-232-45-211.ec2.internal>
2023-08-25 11:28:06 -0400

    added deploy roles for ab2d envs

---
+++ terraform/services/github-actions/deploy-roles/versions.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      business  = "oeda"
+      component = "github-actions"
+      Terraform = true
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    key = "github-runners/terraform.tfstate"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.8.0"
+    }
+  }
+  required_version = "~> 1.5.5"
+}
