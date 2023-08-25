0cbf0df0b53fb311cc946df8665b5a99d6e66347
HK <hk@ip-10-232-45-211.ec2.internal>
2023-08-25 11:28:06 -0400

    added deploy roles for ab2d envs

---
+++ terraform/services/github-actions/deploy-roles/README.md
@@ -0,0 +1,9 @@
+## Instructions
+
+Need to pass backend file when running terraform init
+
+```bash
+terraform init -backend-config=../../../backends/ab2d-dev.s3.tfbackend 
+terraform plan
+terraform apply
+```
---
+++ terraform/services/github-actions/deploy-roles/main.tf
@@ -0,0 +1,43 @@
+data "aws_iam_policy" "developer_boundary_policy" {
+  name = "developer-boundary-policy"
+}
+
+
+resource "aws_iam_role" "runner" {
+  name                 = "github-actions-runner-role"
+  path                 = "/delegatedadmin/developer/"
+  inline_policy {
+    name = "all_within_boundary"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+  permissions_boundary = data.aws_iam_policy.developer_boundary_policy.arn
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = var.runner_arn
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
