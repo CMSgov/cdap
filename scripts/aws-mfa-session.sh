95cec97a176904628e1600a96a68c7c30e236acb
Sean Fern <seanfern@navapbc.com>
2023-08-30 10:14:15 -0400

    Use default profile and improve error handling

--- scripts/aws-mfa-session.sh
+++ scripts/aws-mfa-session.sh
@@ -11,39 +11,47 @@ fi
 
 # https://stackoverflow.com/questions/9901210/bash-source0-equivalent-in-zsh
 # shellcheck disable=SC2296
-if { [ -z "$1" ] || [ -z "$2" ]; }; then
+if { [ -z "$1" ]; }; then
   echo 2>&1 "Usage: source ${BASH_SOURCE:-${(%):-%x}} <account> <profile>"
   echo 2>&1 "Where <account> is the AWS account number and <profile> is the name"
   echo 2>&1 "of the profile with permanent creds in ~/.aws/credentials."
   return 1
 fi
 
-# Read input into variable
+# Read input for MFA code
 printf >&2 '%s ' 'Enter MFA code:'
 read -r mfa_code
 
+profile="${2:-default}"
 tmpfile=$(mktemp)
 
-echo 2>&1 "Getting session token for profile $2"
+cleanup() {
+  rm "$tmpfile"
+  unset mfa_code
+  unset profile
+  unset tmpfile
+}
 
-if ! aws --profile="$2" sts get-session-token --serial-number \
+echo 2>&1 "Getting session token for profile $profile"
+if ! aws --profile="$profile" sts get-session-token --serial-number \
     "arn:aws:iam::$1:mfa/app" --token-code "$mfa_code" > "$tmpfile"; then
   echo 2>&1 "Error getting session token"
+  cleanup
   return 1
 fi
 
-unset mfa_code
+echo 2>&1 "Setting environment variables for AWS session"
+if AWS_ACCESS_KEY_ID="$(jq -r '.Credentials.AccessKeyId' "$tmpfile")" &&
+    AWS_SECRET_ACCESS_KEY="$(jq -r '.Credentials.SecretAccessKey' "$tmpfile")" &&
+    AWS_SESSION_TOKEN="$(jq -r '.Credentials.SessionToken' "$tmpfile")"; then
+  export AWS_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY
+  export AWS_SESSION_TOKEN
+else
+  cleanup
+  return 1
+fi
 
-# Set environment variables
-AWS_ACCESS_KEY_ID="$(jq -r '.Credentials.AccessKeyId' "$tmpfile")"
-AWS_SECRET_ACCESS_KEY="$(jq -r '.Credentials.SecretAccessKey' "$tmpfile")"
-AWS_SESSION_TOKEN="$(jq -r '.Credentials.SessionToken' "$tmpfile")"
-export AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY
-export AWS_SESSION_TOKEN
+echo 2>&1 "Session initialized for profile $profile"
 
-# Remove temp file
-rm "$tmpfile"
-unset tmpfile
-
-echo 2>&1 "Session initialized for profile $2"
+cleanup
