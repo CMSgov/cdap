5b07896ed45787bac261a0abd2416b50ae18cbc0
Sean Fern <seanfern@navapbc.com>
2023-12-15 13:47:59 -0500

    Use impl for vpc stack for ab2d

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,7 +1,7 @@
 data "aws_vpc" "this" {
   filter {
     name   = "tag:stack"
-    values = [var.app_env]
+    values = [var.app_env == "test" && var.app_team == "ab2d" ? "impl" : var.app_env]
   }
   dynamic "filter" {
     for_each = var.app_team == "bcda" || var.app_team == "dpc" ? [1] : []
