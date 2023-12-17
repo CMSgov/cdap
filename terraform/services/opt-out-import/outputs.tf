b6e0f4262c6b6a712132bda75a6680d4294978e5
Sean Fern <seanfern@navapbc.com>
2023-12-17 13:45:36 -0500

    Add bucket name to outputs

--- terraform/services/opt-out-import/outputs.tf
+++ terraform/services/opt-out-import/outputs.tf
@@ -1,3 +1,7 @@
 output "opt_out_import_lambda_role_arn" {
   value = module.opt_out_import_lambda.lambda_role_arn
 }
+
+output "opt_out_import_lambda_bucket" {
+  value = module.opt_out_import_lambda.lambda_zip_file_bucket
+}
