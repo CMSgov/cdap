5b82b4e0422944009be2e6e0f0b9343767c8a1e3
Sean Fern <seanfern@navapbc.com>
2023-12-18 15:19:40 -0500

    Add OIDC role and refactor to use policy docs

--- terraform/services/deploy-roles/main.tf
+++ terraform/services/deploy-roles/main.tf
@@ -2,40 +2,84 @@ data "aws_iam_policy" "developer_boundary_policy" {
   name = "developer-boundary-policy"
 }
 
+#
+# Runner role section
+#
+
+data "aws_iam_policy_document" "runner_assume" {
+  statement {
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [var.runner_arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "runner_inline" {
+  statement {
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
 
 resource "aws_iam_role" "runner" {
   name = "github-actions-runner"
   path = "/delegatedadmin/developer/"
 
   permissions_boundary = data.aws_iam_policy.developer_boundary_policy.arn
-
-  assume_role_policy = jsonencode({
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = var.runner_arn
-        },
-        Action = [
-          "sts:AssumeRole",
-          "sts:TagSession"
-        ]
-      }
-    ]
-  })
+  assume_role_policy   = data.aws_iam_policy_document.runner_assume.json
 
   inline_policy {
-    name = "all-within-boundary"
+    name   = "all-within-boundary"
+    policy = data.aws_iam_policy_document.runner_inline.json
+  }
+}
 
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["*"]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
+#
+# OIDC role section
+#
+
+data "aws_iam_policy_document" "oidc_assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [var.oidc_provider_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:CMSgov/*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "oidc_inline" {
+  statement {
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "oidc" {
+  name = "github-actions-oidc"
+  path = "/delegatedadmin/developer/"
+
+  permissions_boundary = data.aws_iam_policy.developer_boundary_policy.arn
+  assume_role_policy   = data.aws_iam_policy_document.oidc_assume.json
+
+  inline_policy {
+    name   = "all-within-boundary"
+    policy = data.aws_iam_policy_document.oidc_inline.json
   }
 }
