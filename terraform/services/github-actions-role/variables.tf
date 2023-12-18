d178cbdf2a24ea44054baf38a366d72109dd1009
Sean Fern <seanfern@navapbc.com>
2023-12-18 16:11:49 -0500

    Clarify variable descriptions

--- terraform/services/github-actions-role/variables.tf
+++ terraform/services/github-actions-role/variables.tf
@@ -1,9 +1,9 @@
 variable "runner_arn" {
-  description = "The arn for the runner that will assume the role"
+  description = "The arn for the runner role in the mgmt account that may assume this role"
   type        = string
 }
 
 variable "oidc_provider_arn" {
-  description = "The arn for the OIDC provider that will allow the role to be assumed"
+  description = "The arn for the OIDC provider that may allow this role to be assumed from GitHub runners"
   type        = string
 }
