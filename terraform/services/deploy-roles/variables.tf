ba89bfb0c047bef2cc4662a6f10252c728c72877
Sean Fern <seanfern@navapbc.com>
2023-12-18 15:40:01 -0500

    Add variable for OIDC provider ARN

--- terraform/services/deploy-roles/variables.tf
+++ terraform/services/deploy-roles/variables.tf
@@ -2,3 +2,8 @@ variable "runner_arn" {
   description = "The arn for the runner that will assume the role"
   type        = string
 }
+
+variable "oidc_provider_arn" {
+  description = "The arn for the OIDC provider that will allow the role to be assumed"
+  type        = string
+}
