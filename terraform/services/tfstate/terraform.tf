fd8a66b6e76e5891176ec25603cf7d63e0aea82f
Sean Fern <seanfern@navapbc.com>
2023-12-23 13:32:05 -0500

    Rename versions.tf to terraform.tf

--- terraform/services/tfstate/terraform.tf
+++ terraform/services/tfstate/terraform.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
   default_tags {
     tags = {
+      Terraform = true
       business = "oeda"
+      code = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/tfstate"
     }
   }
 }
@@ -12,11 +13,4 @@ terraform {
   #backend "s3" {
   #  key = "tfstate/terraform.tfstate"
   #}
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.8.0"
-    }
-  }
-  required_version = "~> 1.5.5"
 }
