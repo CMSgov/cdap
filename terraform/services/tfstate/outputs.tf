3f17fc141a60a603c8f0cff3932dad4d8119edef
Sean Fern <seanfern@navapbc.com>
2023-12-27 14:03:19 -0500

    PLT-279 Workflows for tfstate service (#32)

--- terraform/services/tfstate/outputs.tf
+++ terraform/services/tfstate/outputs.tf
@@ -5,8 +5,5 @@ output "backend_config" {
   value       = <<EOT
 bucket         = "${aws_s3_bucket.this.id}"
 dynamodb_table = "${aws_dynamodb_table.this.id}"
-region         = "${data.aws_region.current.name}"
-encrypt        = true
-kms_key_id     = "${aws_kms_alias.this.name}"
 EOT
 }
