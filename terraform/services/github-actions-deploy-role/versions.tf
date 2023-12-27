2ee9238f6142191a41653ca1d031ac805ce190b6
Sean Fern <seanfern@navapbc.com>
2023-12-27 15:21:23 -0500

    Add code tag

--- terraform/services/github-actions-deploy-role/versions.tf
+++ terraform/services/github-actions-deploy-role/versions.tf
@@ -3,6 +3,7 @@ provider "aws" {
   default_tags {
     tags = {
       business  = "oeda"
+      code      = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/github-actions-deploy-role"
       component = "github-actions"
       terraform = true
     }
