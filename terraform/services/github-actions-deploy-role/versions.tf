4a6c3dd6c996e8961f79ba060f01da0273e4793b
Sean Fern <seanfern@navapbc.com>
2023-12-27 15:22:25 -0500

    Drop region and required from versions.tf

--- terraform/services/github-actions-deploy-role/versions.tf
+++ terraform/services/github-actions-deploy-role/versions.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  region = "us-east-1"
   default_tags {
     tags = {
       business  = "oeda"
@@ -14,11 +13,4 @@ terraform {
   backend "s3" {
     key = "github-actions-deploy-role/terraform.tfstate"
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.8.0"
-    }
-  }
-  required_version = "~> 1.5.5"
 }
