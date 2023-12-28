9ac7eb19232122de53a4520c2269d24f10659ee4
Sean Fern <seanfern@navapbc.com>
2023-12-28 20:55:01 +0000

    PLT-278 GitHub Actions deploy role workflows (#33)

--- terraform/services/github-actions-deploy-role/terraform.tf
+++ terraform/services/github-actions-deploy-role/terraform.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
   default_tags {
     tags = {
       business  = "oeda"
+      code      = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/github-actions-deploy-role"
       component = "github-actions"
-      Terraform = true
+      terraform = true
     }
   }
 }
@@ -13,11 +13,4 @@ terraform {
   backend "s3" {
     key = "github-actions-deploy-role/terraform.tfstate"
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.8.0"
-    }
-  }
-  required_version = "~> 1.5.5"
 }
