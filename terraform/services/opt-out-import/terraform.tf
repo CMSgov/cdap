f936bd093dcd46876aaf88e56d219757b502d66a
Sean Fern <seanfern@navapbc.com>
2024-01-09 15:17:18 -0500

    Add application and environment tags

--- terraform/services/opt-out-import/terraform.tf
+++ terraform/services/opt-out-import/terraform.tf
@@ -1,10 +1,12 @@
 provider "aws" {
   default_tags {
     tags = {
-      business  = "oeda"
-      code      = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/opt-out-import"
-      component = "opt-out-import"
-      terraform = true
+      application = var.app_team
+      business    = "oeda"
+      code        = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/opt-out-import"
+      component   = "opt-out-import"
+      environment = var.app_env
+      terraform   = true
     }
   }
 }
