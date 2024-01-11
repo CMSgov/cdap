41b48ffeb55d973724fb2f9d29de69c8fa11f425
Grant Freeman <grantfreeman@navapbc.com>
2024-01-11 13:58:04 -0500

    revert to old image

--- terraform/services/github-actions-runner/main.tf
+++ terraform/services/github-actions-runner/main.tf
@@ -80,7 +80,7 @@ module "github-actions-runner" {
   role_permissions_boundary = data.aws_iam_policy.developer_boundary_policy.arn
 
   enable_ssm_on_runners = true
-  enable_userdata       = false
+  enable_userdata       = true
 
   idle_config = [{
     cron             = "* * 9-17 * * 1-5"
