f845a41396ae13c9ec9f0b350edaba1a713e4029
Sean Fern <seanfern@navapbc.com>
2024-02-20 01:52:07 -0500

    Add opt-out-import outputs back

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -2,3 +2,13 @@ output "function_name" {
   description = "Name for the lambda function"
   value = aws_lambda_function.this.function_name
 }
+
+output "role_arn" {
+  description = "ARN of the IAM role for the lambda"
+  value       = aws_iam_role.lambda.arn
+}
+
+output "zip_bucket" {
+  description = "Bucket name for the function.zip file"
+  value       = module.zip_bucket.id
+}
