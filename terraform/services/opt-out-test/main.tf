3f5893b18d9afdfaf006c87daae4728e0b311456
Sean Fern <seanfern@navapbc.com>
2024-02-27 01:40:03 -0500

    Add bucket notification resource

--- terraform/services/opt-out-test/main.tf
+++ terraform/services/opt-out-test/main.tf
@@ -8,3 +8,13 @@ module "test_topic" {
   name    = "${var.app}-${var.env}-opt-out-test"
   buckets = [module.test_bucket.arn]
 }
+
+resource "aws_s3_bucket_notification" "this" {
+  bucket = module.test_bucket.id
+
+  topic {
+    topic_arn     = module.test_topic.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "oot01/"
+  }
+}
