6e0813e7c9b025f35ff488e5d323b3b98f0e1a8d
Sean Fern <seanfern@navapbc.com>
2024-02-28 01:28:43 -0500

    Fix reference for access logs policy

--- terraform/modules/bucket/main.tf
+++ terraform/modules/bucket/main.tf
@@ -95,7 +95,7 @@ resource "aws_s3_bucket" "access_logs" {
 }
 
 resource "aws_s3_bucket_policy" "access_logs" {
-  bucket = aws_s3_bucket.this.id
+  bucket = aws_s3_bucket.access_logs.id
   policy = data.aws_iam_policy_document.tls_only.json
 }
 
