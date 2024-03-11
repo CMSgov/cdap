a7a2897dd03d8fa0d2f0512fc66ae89c857d2a7d
Kevin Yeh <kevin@navapbc.com>
2024-03-10 23:23:32 -0400

    Add ssm:GetParameter permissions to lambdas

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -103,6 +103,7 @@ data "aws_iam_policy_document" "function_inline" {
       "sqs:DeleteMessage",
       "sqs:GetQueueAttributes",
       "sqs:ReceiveMessage",
+      "ssm:GetParameter",
       "ssm:GetParameters",
     ]
     resources = ["*"]
