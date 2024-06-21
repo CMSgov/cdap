66829e41680c566b2b25c1dd1dfb37fa93d7ed21
Maboh Christopher <chrismaboh@Mabohs-MBP.lan>
2024-06-21 14:38:33 -0400

    terraform fmt

--- terraform/services/bucket-access-log/terraform.tf
+++ terraform/services/bucket-access-log/terraform.tf
@@ -2,11 +2,11 @@ provider "aws" {
   default_tags {
     tags = {
       application = var.app
-      business  = "oeda"
-      code      = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/bucket-access-log"
-      component = "bucket-access-log"
+      business    = "oeda"
+      code        = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/bucket-access-log"
+      component   = "bucket-access-log"
       environment = var.env
-      terraform = true
+      terraform   = true
     }
   }
 }
