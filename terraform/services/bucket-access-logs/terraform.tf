858c5c29d27f4c7874d3d403343b0e65dd324f1c
Maboh Christopher <chrismaboh@Mabohs-MBP.lan>
2024-07-12 13:35:22 -0400

    app and env variables removed

--- terraform/services/bucket-access-logs/terraform.tf
+++ terraform/services/bucket-access-logs/terraform.tf
@@ -1,12 +1,10 @@
 provider "aws" {
   default_tags {
     tags = {
-      application = var.app
-      business    = "oeda"
-      code        = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/bucket-access-logs"
-      component   = "bucket-access-logs"
-      environment = var.env
-      terraform   = true
+      business  = "oeda"
+      code      = "https://github.com/CMSgov/ab2d-bcda-dpc-platform/tree/main/terraform/services/bucket-access-logs"
+      component = "bucket-access-logs"
+      terraform = true
     }
   }
 }
