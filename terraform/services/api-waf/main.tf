a47eff5b027bac55be90061d2f04579a83283cd0
Grant Freeman <grantfreeman@navapbc.com>
2024-07-17 09:10:55 -0700

    formatting

--- terraform/services/api-waf/main.tf
+++ terraform/services/api-waf/main.tf
@@ -1,9 +1,9 @@
 module "aws_waf" {
   source = "../../modules/firewall"
 
-  app = var.app
-  env = var.env
-  aws_lb_arn = var.aws_lb_arn
+  app             = var.app
+  env             = var.env
+  aws_lb_arn      = var.aws_lb_arn
   rate_based_rule = var.rate_based_rule
-  ip_sets_rule = var.ip_sets_rule
+  ip_sets_rule    = var.ip_sets_rule
 }
