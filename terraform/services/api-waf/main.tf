79e031f684720d770c7a41d225319450dbfa2f15
Grant Freeman <grantfreeman@navapbc.com>
2024-07-18 11:13:46 -0700

    add load balancer selection

--- terraform/services/api-waf/main.tf
+++ terraform/services/api-waf/main.tf
@@ -1,9 +1,28 @@
+locals {
+  load_balancers = {
+    ab2d = [
+      "api-${var.app}-east-${var.env}",
+    ]
+    bcda = [
+      "${var.app}-api-${var.env}-01",
+    ]
+    dpc = [
+      "${var.app}-${var.env}-1",
+    ]
+  }
+}
+
+data "aws_lb" "aws_lb_arn" {
+  name = local.load_balancers[var.app]
+}
+
 module "aws_waf" {
   source = "../../modules/firewall"
 
   app             = var.app
   env             = var.env
-  aws_lb_arn      = var.aws_lb_arn
+  aws_lb_arn      = aws_lb_arn.arn
   rate_based_rule = var.rate_based_rule
   ip_sets_rule    = var.ip_sets_rule
+  region          = var.region
 }
--- terraform/services/api-waf/variables.tf
+++ terraform/services/api-waf/variables.tf
@@ -21,10 +21,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "aws_lb_arn" {
-  description = "ARN of the LoadBalancer to attach the WAF to"
-}
-
 variable "rate_based_rule" {
   type = object({
     name          = string
