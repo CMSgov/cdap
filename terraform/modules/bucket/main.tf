6181efb47834a445294806738407ef13ab1a7927
christopher  Maboh <christopher.maboh@yahoo.com>
2024-07-23 13:18:59 -0400

    switch resource block to data block (#96)
    Co-authored-by: Maboh Christopher <chrismaboh@Mabohs-MBP.lan>

--- terraform/modules/bucket/main.tf
+++ terraform/modules/bucket/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 module "bucket_key" {
   source      = "../key"
   name        = "${var.name}-bucket"
@@ -85,64 +87,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     }
   }
 }
-
-resource "aws_s3_bucket" "access_logs" {
-  bucket        = "${var.name}-access"
-  force_destroy = true
+data "aws_s3_bucket" "bucket_access_logs" {
+  bucket = "${data.aws_caller_identity.current.account_id}-bucket-access-logs"
 }
-
-data "aws_iam_policy_document" "access_logs" {
-  statement {
-    sid = "AllowSSLRequestsOnly"
-
-    effect = "Deny"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = ["s3:*"]
-
-    resources = [
-      aws_s3_bucket.access_logs.arn,
-      "${aws_s3_bucket.access_logs.arn}/*",
-    ]
-
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "access_logs" {
-  bucket = aws_s3_bucket.access_logs.id
-  policy = data.aws_iam_policy_document.access_logs.json
-}
-
-resource "aws_s3_bucket_versioning" "access_logs" {
-  bucket = aws_s3_bucket.access_logs.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "access_logs" {
-  bucket = aws_s3_bucket.access_logs.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = module.bucket_key.id
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
 resource "aws_s3_bucket_logging" "this" {
   bucket = aws_s3_bucket.this.id
 
-  target_bucket = aws_s3_bucket.access_logs.id
-  target_prefix = "log/"
+  target_bucket = data.aws_s3_bucket.bucket_access_logs.id
+  target_prefix = "${var.name}/"
 }
--- terraform/modules/bucket/variables.tf
+++ terraform/modules/bucket/variables.tf
@@ -5,6 +5,6 @@ variable "name" {
 
 variable "cross_account_read_roles" {
   description = "Roles in other accounts that need read access to this S3 bucket"
-  type        = list
+  type        = list(any)
   default     = []
 }
