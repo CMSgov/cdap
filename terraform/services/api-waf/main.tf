770080a2c35fb679ac6f400bdb795388b3ebd32c
Sean Fern <seanfern@navapbc.com>
2024-08-25 13:58:29 -0400

    Add support for sbx env

--- terraform/services/api-waf/main.tf
+++ terraform/services/api-waf/main.tf
@@ -2,23 +2,18 @@ locals {
   ab2d_env_lbs = {
     dev  = "ab2d-dev"
     test = "ab2d-east-impl"
+    sbx  = "ab2d-sbx-sandbox"
     prod = "api-ab2d-east-prod"
   }
   load_balancers = {
-    ab2d = {
-      name = "${local.ab2d_env_lbs[var.env]}"
-    }
-    bcda = {
-      name = "${var.app}-api-${var.env}-01"
-    }
-    dpc = {
-      name = "${var.app}-${var.env}-1"
-    }
+    ab2d = "${local.ab2d_env_lbs[var.env]}"
+    bcda = "bcda-api-${var.env == "sbx" ? "opensbx" : var.env}-01"
+    dpc  = "dpc-${var.env == "sbx" ? "prod-sbx" : var.env}-1"
   }
 }
 
 data "aws_lb" "api_lb" {
-  name = local.load_balancers[var.app].name
+  name = local.load_balancers[var.app]
 }
 
 module "aws_waf" {
