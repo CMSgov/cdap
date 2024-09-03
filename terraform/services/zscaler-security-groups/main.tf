71380397a8dee39470eaae0976b9fee6608d37f0
Sean Fern <seanfern@navapbc.com>
2024-09-02 23:35:59 -0400

    Add internet security group

--- terraform/services/zscaler-security-groups/main.tf
+++ terraform/services/zscaler-security-groups/main.tf
@@ -12,12 +12,32 @@ resource "aws_security_group" "zscaler_public" {
 
 resource "aws_security_group" "zscaler_private" {
   name        = "${var.app}-${var.env}-allow-zscaler-private"
-  description = "Allow internet zscaler traffic private"
+  description = "Allow private zscaler traffic"
   vpc_id      = module.vpc.id
 }
 
-resource "aws_security_group" "packer" {
-  name        = "${var.app}-${var.env}-packer"
-  description = "Access for packer instances"
+resource "aws_security_group" "internet" {
+  name        = "${var.app}-${var.env}-internet"
+  description = "Access to the internet"
   vpc_id      = module.vpc.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "internet_http" {
+  security_group_id = aws_security_group.packer.id
+
+  description = "Allow http access to the internet"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "internet_https" {
+  security_group_id = aws_security_group.packer.id
+
+  description = "Allow https access to the internet"
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 }
