920523b38494470b069abeb970d16a552c4a720b
Sean Fern <seanfern@navapbc.com>
2024-09-02 22:00:09 -0400

    Add packer sg

--- terraform/services/zscaler-security-groups/main.tf
+++ terraform/services/zscaler-security-groups/main.tf
@@ -1,20 +1,23 @@
-### Get vpc reference
 module "vpc" {
   source = "../../modules/vpc"
   app    = var.app
   env    = var.env
 }
 
-### public
 resource "aws_security_group" "zscaler_public" {
   name        = "${var.app}-${var.env}-allow-zscaler-public"
   description = "Allow public zscaler traffic"
   vpc_id      = module.vpc.id
 }
 
-### private
 resource "aws_security_group" "zscaler_private" {
   name        = "${var.app}-${var.env}-allow-zscaler-private"
   description = "Allow internet zscaler traffic private"
+  vpc_id      = module.vpc.id
+}
+
+resource "aws_security_group" "packer" {
+  name        = "${var.app}-${var.env}-packer"
+  description = "Access for packer instances"
   vpc_id      = module.vpc.id
 }
