9586d004669ecc4c058c6e85ee1c534ca160b024
HK <hunkeelin@navapbc.com>
2024-09-10 10:48:49 -0700

    dev is clear

--- terraform/services/api-rds/main.tf
+++ terraform/services/api-rds/main.tf
@@ -1,10 +1,25 @@
 resource "aws_db_instance" "api" {
-  identifier            = "${var.env}-${var.app}"
-  allocated_storage     = 10
-  max_allocated_storage = 100
-  storage_type          = "gp2"
-  engine                = "postgres"
-  engine_version        = "11"
-  instance_class        = "db.m6i.large"
-  skip_final_snapshot   = false
+  identifier            = "${var.app}-${var.env}"
+  allocated_storage     = 500
+  max_allocated_storage = 0
+  storage_encrypted     = true
+  enabled_cloudwatch_logs_exports = [
+    "postgresql",
+    "upgrade",
+  ]
+  deletion_protection                 = true
+  storage_type                        = "io1"
+  skip_final_snapshot                 = true
+  engine                              = "postgres"
+  iam_database_authentication_enabled = false
+  engine_version                      = "15.5"
+  instance_class                      = "db.m6i.2xlarge"
+  tags = {
+    Name             = "${var.app}-${var.env}-rds"
+    "cpm backup"     = "Monthly"
+    contact          = "ab2d-ops@semanticbits.com"
+    environment      = "${var.app}-${var.env}"
+    role             = "db"
+    terraform_module = "data"
+  }
 }
