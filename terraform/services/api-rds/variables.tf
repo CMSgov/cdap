e34d9c858acd59667b5ceafd500ba160c4eda650
ildesenesence <31672538+ildesenesence@users.noreply.github.com>
2024-10-11 16:50:11 -0500

    PLT-691 Apply api rds service to ab2d test (#134)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-691
    
    ## 🛠 Changes
    
    Added management of AB2D Test environment to api-rds service in Platform
    repository
    Imported existing resources to state file to track managed components
    
    ## ℹ️ Context
    
    Changes were made to centralize database management under PLT-691 and
    PLT-563
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    Clean terraform plans will resolve locally and will be shown in GHA
    tasks
    
    ---------
    
    Co-authored-by: HK <hunkeelin@navapbc.com>
    Co-authored-by: Sean Fern <seanfern@navapbc.com>

--- terraform/services/api-rds/variables.tf
+++ terraform/services/api-rds/variables.tf
@@ -1,3 +1,5 @@
+# Restricting valid "app" values until service has been extended or BCDA and DPC
+
 variable "app" {
   description = "The application name (ab2d, bcda, dpc)"
   type        = string
@@ -14,4 +16,10 @@ variable "env" {
     condition     = contains(["dev", "test", "sbx", "prod"], var.env)
     error_message = "Valid value for env is dev, test, sbx, or prod."
   }
+}
+
+variable "jenkins_security_group_id" {
+  description = "Stores the security group managing Jenkins Agent for AB2D including account number for AB2D Management"
+  type        = string
+  nullable    = false
 }
