d4038362dbf47801651d344f574fe0cfc426c415
timothy-ellis-ky <tim.ellis@semanticbits.com>
2024-10-30 15:06:10 -0400

    serialization option 3

--- terraform/services/dpc-quicksights/glue.tf
+++ terraform/services/dpc-quicksights/glue.tf
@@ -56,7 +56,8 @@ locals {
     },
     parquet = {
       #library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
-      library = "org.openx.data.jsonserde.JsonSerDe"
+      #library = "org.openx.data.jsonserde.JsonSerDe"
+      library = "com.amazon.ionhiveserde.IonHiveSerDe"
       params = {
         "serialization.format" = 1
       }
