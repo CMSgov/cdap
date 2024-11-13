d3e8459aa334d4e6ebb1c44712bd9e07b9c4a4f3
timothy-ellis-ky <tim.ellis@semanticbits.com>
2024-11-13 11:49:51 -0500

    add bucket module key outputs

--- terraform/modules/bucket/outputs.tf
+++ terraform/modules/bucket/outputs.tf
@@ -7,3 +7,18 @@ output "id" {
   description = "ID for the S3 bucket"
   value       = aws_s3_bucket.this.id
 }
+
+output "key_alias" {
+  description = "Key Alias for this bucket"
+  value       = module.bucket_key.alias
+}
+
+output "key_arn" {
+  description = "KEY ARN for this bucket"
+  value       = module.bucket_key.arn
+}
+
+output "key_id" {
+  description = "KEY identifier for this bucket"
+  value       = module.bucket_key.id
+}
