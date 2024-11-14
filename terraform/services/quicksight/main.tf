76d41ed9aca8177b526c6c8ea0baf2b0bfd696b0
timothy-ellis-ky <tim.ellis@semanticbits.com>
2024-11-14 11:31:12 -0500

    athena apply errors

--- terraform/services/quicksight/main.tf
+++ terraform/services/quicksight/main.tf
@@ -26,9 +26,11 @@ locals {
   dpc_glue_bucket_arn         = module.dpc_insights_data.arn
   dpc_glue_bucket_key_alias   = module.dpc_insights_data.key_alias
   dpc_glue_bucket_key_arn     = module.dpc_insights_data.key_arn
+  dpc_glue_bucket_key_id      = mmodule.dpc_insights_data.id
   dpc_athena_bucket_arn       = module.dpc_insights_athena.arn
   dpc_athena_bucket_key_alias = module.dpc_insights_athena.key_alias
   dpc_athena_bucket_key_arn   = module.dpc_insights_athena.key_arn
+  dpc_athena_bucket_key_id    = module.dpc_insights_data.id
 }
 
 data "aws_caller_identity" "current" {}
