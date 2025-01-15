02e1b3ab5715f29a391c98f9262dc9822a97d3eb
carlpartridge <carlpartridge@navapbc.com>
2025-01-15 10:54:59 -0500

    Correct module name

--- terraform/services/admin-aco-deny/outputs.tf
+++ terraform/services/admin-aco-deny/outputs.tf
@@ -1,7 +1,7 @@
 output "function_role_arn" {
-  value = module.admin_aco_deny.role_arn
+  value = module.admin_aco_deny_function.role_arn
 }
 
 output "zip_bucket" {
-  value = module.admin_aco_deny.zip_bucket
+  value = module.admin_aco_deny_function.zip_bucket
 }
