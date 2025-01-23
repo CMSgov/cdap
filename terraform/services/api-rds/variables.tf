3d67022ad14ac326e0971c5e353a985d781cd8ad
Maboh Christopher <chris.maboh@aquia.io>
2025-01-23 09:53:15 -0500

    incorporate bcda to ab2d terraform

--- terraform/services/api-rds/variables.tf
+++ terraform/services/api-rds/variables.tf
@@ -13,8 +13,8 @@ variable "env" {
   description = "The application environment (dev, test, sbx, prod)"
   type        = string
   validation {
-    condition     = contains(["dev", "test", "sbx", "prod"], var.env)
-    error_message = "Valid value for env is dev, test, sbx, or prod."
+    condition     = contains(["dev", "test", "opensbx", "prod"], var.env)
+    error_message = "Valid value for env is dev, test, opensbx, or prod."
   }
 }
 
