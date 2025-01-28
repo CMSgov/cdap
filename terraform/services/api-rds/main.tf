3a27bb0e8d0f82d8fb2f0e5cfe65e8b800b4b134
Maboh Christopher <chris.maboh@aquia.io>
2025-01-28 11:30:12 -0500

    incorporate bcda to ab2d terraform

--- terraform/services/api-rds/main.tf
+++ terraform/services/api-rds/main.tf
@@ -54,7 +54,7 @@ resource "aws_vpc_security_group_egress_rule" "egress_all" {
   ip_protocol = -1
 }
 
-/*resource "aws_vpc_security_group_ingress_rule" "db_access_from_jenkins_agent" {
+resource "aws_vpc_security_group_ingress_rule" "db_access_from_jenkins_agent" {
   for_each = var.app == "bcda" ? toset([data.aws_security_group.app_sg[0].id, data.aws_security_group.worker_sg[0].id, var.jenkins_security_group_id]) : var.app == "ab2d" ? toset([var.jenkins_security_group_id]) : toset([])
 
   description                  = "Jenkins Agent Access"
@@ -63,28 +63,7 @@ resource "aws_vpc_security_group_egress_rule" "egress_all" {
   ip_protocol                  = "tcp"
   referenced_security_group_id = each.value
   security_group_id            = aws_security_group.sg_database.id
-}*/
-resource "aws_vpc_security_group_ingress_rule" "db_access_from_jenkins_agent" {
-  for_each = var.app == "bcda" ? toset([
-    "app_sg",
-    "worker_sg",
-    "jenkins_sg"
-  ]) : var.app == "ab2d" ? toset(["jenkins_sg"]) : toset([])
-
-  description                  = "Jenkins Agent Access"
-  from_port                    = 5432
-  to_port                      = 5432
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = data.aws_security_group.app_sg[0].id
-  security_group_id            = aws_security_group.sg_database.id
-
-  lifecycle {
-    ignore_changes = [
-      referenced_security_group_id
-    ]
-  }
 }
-
 
 resource "aws_vpc_security_group_ingress_rule" "db_access_from_controller" {
   count                        = var.app == "ab2d" ? 1 : 0
