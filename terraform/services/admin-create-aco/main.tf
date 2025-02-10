01bfc7771bdc35f8e9719d53b2e9694ac40f182f
austincanada <162146803+austincanada@users.noreply.github.com>
2025-02-10 16:54:24 -0500

    BCDA-8629: Add infrastructure for Create ACO Admin Task (#176)
    In order for BCDA to move off of Jenkins we need a new lambda to handle
    the admin task of Create ACO. This terraform service will handle the IaC
    side.
    
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/BCDA-8629
    
    ## 🛠 Changes
    
    Added a new service for the platform
    
    ## ℹ️ Context
    
    Infrastructure needed to be made in order to handle the new lambda.
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    Terraform plan

---
+++ terraform/services/admin-create-aco/main.tf
@@ -0,0 +1,41 @@
+locals {
+  full_name   = "${var.app}-${var.env}-admin-create-aco"
+  db_sg_name  = "bcda-${var.env == "sbx" ? "opensbx" : var.env}-rds"
+  memory_size = 256
+}
+
+module "admin_create_aco_function" {
+  source = "../../modules/function"
+
+  app = var.app
+  env = var.env
+
+  name        = local.full_name
+  description = "Creates an ACO for BCDA."
+
+  handler = "bootstrap"
+  runtime = "provided.al2"
+
+  memory_size = local.memory_size
+
+  environment_variables = {
+    ENV      = var.env
+    APP_NAME = "${var.app}-${var.env}-admin-create-aco"
+  }
+}
+
+# Add a rule to the database security group to allow access from the function
+data "aws_security_group" "db" {
+  name = local.db_sg_name
+}
+
+resource "aws_security_group_rule" "function_access" {
+  type        = "ingress"
+  from_port   = 5432
+  to_port     = 5432
+  protocol    = "tcp"
+  description = "admin-create-aco function access"
+
+  security_group_id        = data.aws_security_group.db.id
+  source_security_group_id = module.admin_create_aco_function.security_group_id
+}
