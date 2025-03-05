465ca03ad0f254d4d2ed9712d13e8426c1cc4f7f
Maboh Christopher <chris.maboh@aquia.io>
2025-03-05 13:54:06 -0500

    add bcda to app

--- terraform/services/api-rds/variables.tf
+++ terraform/services/api-rds/variables.tf
@@ -28,10 +28,3 @@ variable "mgmt_vpc_cidr" {
   description = "CIDR for the Management VPC"
   type        = string
 }
-
-# These CIDR blocks represent US based AWS QuickSight servers
-variable "quicksight_cidr_blocks" {
-  description = "CIDR blocks for AWS QuickSight IP ranges"
-  type        = list(string)
-  default     = []
-}
