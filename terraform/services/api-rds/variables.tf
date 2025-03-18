9a8b16ceee9937867e95cfe096d0d29891705a23
Maboh Christopher <chris.maboh@aquia.io>
2025-03-18 13:14:27 -0400

    change environment name from sbx to opensbx

--- terraform/services/api-rds/variables.tf
+++ terraform/services/api-rds/variables.tf
@@ -10,11 +10,11 @@ variable "app" {
 }
 
 variable "env" {
-  description = "The application environment (dev, test, sbx, prod)"
+  description = "The application environment (dev, test, opensbx, prod)"
   type        = string
   validation {
     condition     = contains(["dev", "test", "opensbx", "prod"], var.env)
-    error_message = "Valid value for env is dev, test, sbx, or prod."
+    error_message = "Valid value for env is dev, test, opensbx, or prod."
   }
 }
 
