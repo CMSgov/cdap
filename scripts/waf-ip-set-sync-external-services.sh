a6c2b41b3cc34eb5b7c8d6b1162f1b2d0cb32dea
Julian Scott <julianscott@navapbc.com>
2025-03-21 14:23:03 -0700

    Fixing IP list jq syntax.

--- scripts/waf-ip-set-sync-external-services.sh
+++ scripts/waf-ip-set-sync-external-services.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-IPV4_LIST=$(grep -v '^#' temp/ip-sets/external-services/allowed_ips.txt | jq -Rs '{Addresses: split("\n") | map(select(length > 0))}')
+IPV4_LIST=$(grep -v '^#' temp/ip-sets/external-services/allowed_ips.txt | jq -Rs '{Addresses: split("\n") | map(select(length > 0))}' | jq -rc .Addresses)
 IPV4_SET_ID=$(aws wafv2 list-ip-sets --scope REGIONAL --region $AWS_REGION | jq -r '.IPSets[] | select( .Name=="external-services") | .Id')
 
 LOCK_TOKEN=$(aws wafv2 get-ip-set --name external-services --scope REGIONAL --id $IPV4_SET_ID --region us-east-1 | jq -r '.LockToken')
