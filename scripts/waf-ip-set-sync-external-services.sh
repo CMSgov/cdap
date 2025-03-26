6373b32c8f0275db2f0a11e3355142e804f9f0c7
Julian Scott <julianscott@navapbc.com>
2025-03-26 15:38:29 -0700

    Removing debug info from external services sync script.

--- scripts/waf-ip-set-sync-external-services.sh
+++ scripts/waf-ip-set-sync-external-services.sh
@@ -7,18 +7,12 @@ IPV4_SET_ID=$(aws wafv2 list-ip-sets --scope REGIONAL --region $AWS_REGION | jq 
 
 LOCK_TOKEN=$(aws wafv2 get-ip-set --name external-services --scope REGIONAL --id $IPV4_SET_ID --region us-east-1 | jq -r '.LockToken')
 
-echo "Beginning 'external services' regional IPv4 set update."
+echo "Beginning 'external services' IPv4 set update."
 
-echo "IPV4_SET_ID is ${IPV4_SET_ID}"
-echo "App/Env are ${APP} ${ENV}"
-
-SAMPLE_NAME=$(aws ec2 describe-instances | jq -r '.Reservations[0].Instances[].Tags[] | select( .Key=="Name") | .Value')
-echo "Sample name is ${SAMPLE_NAME}"
-
-# aws wafv2 update-ip-set \
-#   --name external-services \
-#   --scope REGIONAL \
-#   --id $IPV4_SET_ID \
-#   --region us-east-1 \
-#   --addresses $IPV4_LIST \
-#   --lock-token $LOCK_TOKEN
+aws wafv2 update-ip-set \
+  --name external-services \
+  --scope REGIONAL \
+  --id $IPV4_SET_ID \
+  --region us-east-1 \
+  --addresses $IPV4_LIST \
+  --lock-token $LOCK_TOKEN
