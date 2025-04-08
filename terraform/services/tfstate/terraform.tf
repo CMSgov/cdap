bb700adcd64fe30aaefaab0f5fdcfeba8cd18b1c
juliareynolds-nava <juliareynolds@navapbc.com>
2025-04-08 10:35:10 -0600

    revert changes per SF

--- terraform/services/tfstate/terraform.tf
+++ terraform/services/tfstate/terraform.tf
@@ -7,17 +7,10 @@ provider "aws" {
     }
   }
 }
-resource "aws_s3_bucket" "backend_bucket" {
-  bucket = "${var.app}-${var.env}-tfstate"
-}
 
 terraform {
   # Comment out backend block and init without -backend-config for initial creation of resources
   backend "s3" {
-    key            = "tfstate/terraform.tfstate"
-    bucket         = ""
-    dynamodb_table = ""
-    encrypt        = true
-
+    key = "tfstate/terraform.tfstate"
   }
 }
