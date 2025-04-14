ac2f8d7e6cec317352e307853b3c8323e1898c63
juliareynolds-nava <juliareynolds@navapbc.com>
2025-04-14 12:54:26 -0600

    add use_lockfile to backend s3

--- terraform/services/tfstate/terraform.tf
+++ terraform/services/tfstate/terraform.tf
@@ -11,6 +11,7 @@ provider "aws" {
 terraform {
   # Comment out backend block and init without -backend-config for initial creation of resources
   backend "s3" {
-    key = "tfstate/terraform.tfstate"
+    key          = "tfstate/terraform.tfstate"
+    use_lockfile = true
   }
 }
