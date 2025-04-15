596cd991878347e7d088c3d54fc3f58c8694a276
juliareynolds-nava <juliareynolds@navapbc.com>
2025-04-15 10:54:01 -0600

    restore mgmt env

--- terraform/services/tfstate/variables.tf
+++ terraform/services/tfstate/variables.tf
@@ -1,17 +1,17 @@
 variable "app" {
-  description = "The application name (ab2d, bcda, dpc)"
+  description = "The application name (ab2d, bcda, dpc, cdap)"
   type        = string
   validation {
-    condition     = contains(["ab2d", "bcda", "dpc"], var.app)
-    error_message = "Valid value for app is ab2d, bcda, or dpc."
+    condition     = contains(["ab2d", "bcda", "dpc", "cdap"], var.app)
+    error_message = "Valid value for app is ab2d, bcda, cdap, or dpc."
   }
 }
 
 variable "env" {
-  description = "The application environment (dev, test, sbx, prod)"
+  description = "The application environment (dev, test, sbx, prod, mgmt)"
   type        = string
   validation {
-    condition     = contains(["dev", "test", "sbx", "prod"], var.env)
+    condition     = contains(["dev", "test", "sbx", "prod", "mgmt"], var.env)
     error_message = "Valid value for env is dev, test, sbx, or prod."
   }
 }
