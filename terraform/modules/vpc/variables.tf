933835b76c0a296dcef68242617b115db7016058
juliareynolds-nava <juliareynolds@navapbc.com>
2025-04-16 11:20:07 -0600

    correct terraform format

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -8,10 +8,10 @@ variable "app" {
 }
 
 variable "env" {
-  description = "The application environment (dev, test, sbx, sandbox, prodm mgmt)"
+  description = "The application environment (dev, test, sbx, sandbox, prod, mgmt)"
   type        = string
   validation {
-    condition     = contains(["dev", "test", "sbx", "sandbox","prod", "mgmt"], var.env)
+    condition     = contains(["dev", "test", "sbx", "sandbox", "prod", "mgmt"], var.env)
     error_message = "Valid value for env is dev, test, sbx, sandbox, mgmt or prod."
   }
 }
