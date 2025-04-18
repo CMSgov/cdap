3b86cb05a8872771e1f6075cffa1d93099babc60
Sean Fern <seanfern@navapbc.com>
2025-04-18 09:22:53 -0400

    Add cdap and sandbox for greenfield

--- terraform/services/github-actions-role/variables.tf
+++ terraform/services/github-actions-role/variables.tf
@@ -1,17 +1,17 @@
 variable "app" {
-  description = "The application name (ab2d, bcda, dpc)"
+  description = "The application name (ab2d, bcda, dpc, cdap)"
   type        = string
   validation {
-    condition     = contains(["ab2d", "bcda", "dpc"], var.app)
-    error_message = "Valid value for app is ab2d, bcda, or dpc."
+    condition     = contains(["ab2d", "bcda", "dpc", "cdap"], var.app)
+    error_message = "Valid value for app is ab2d, bcda, dpc, or cdap."
   }
 }
 
 variable "env" {
-  description = "The application environment (dev, test, mgmt, sbx, prod)"
+  description = "The application environment (dev, test, mgmt, sbx, sandbox, prod)"
   type        = string
   validation {
-    condition     = contains(["dev", "test", "mgmt", "sbx", "prod"], var.env)
-    error_message = "Valid value for env is dev, test, mgmt, sbx, or prod."
+    condition     = contains(["dev", "test", "mgmt", "sbx", "sandbox", "prod"], var.env)
+    error_message = "Valid value for env is dev, test, mgmt, sbx, sandbox, or prod."
   }
 }
