1c7545cea829286c8560f4f6cc98c25c430e0c9e
Sean Fern <seanfern@navapbc.com>
2025-05-15 15:16:52 -0400

    Revert "Drop waf-ip-set-sync scripts"
    This reverts commit 47c3c5167e7f1f0bb478b1783ad7ef2743ccfee9.
    The legacy workflows are still using these scripts.

---
+++ scripts/waf-ip-set-sync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Generating IP set"
+
+IPV4_LIST=$(grep -v '^#' temp/ip-sets/ab2d/allowed_ips.txt | jq -Rs '{Addresses: split("\n") | map(select(length > 0))}' | jq -rc .Addresses)
+
+echo "Fetching IP set IDs"
+
+{
+  read -r IPV4_SET_ID
+  read -r IPV6_SET_ID
+} < <(aws wafv2 list-ip-sets --scope REGIONAL | jq -r '.IPSets[] | select( .Name | contains("api-customers")) | .Id')
+
+echo "Updating IPv4 set"
+
+LOCK_TOKEN=$(aws wafv2 get-ip-set --name $APP-$ENV-api-customers --scope REGIONAL --id $IPV4_SET_ID --region us-east-1 | jq -r '.LockToken')
+
+aws wafv2 update-ip-set \
+  --name $APP-$ENV-api-customers \
+  --scope REGIONAL \
+  --id $IPV4_SET_ID \
+  --region us-east-1 \
+  --addresses $IPV4_LIST \
+  --lock-token $LOCK_TOKEN \
+  > /dev/null 2>&1
