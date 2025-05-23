73cf2fa33c43b73f295bdfad7ef011771dae2b2b
Sean Fern <seanfern@navapbc.com>
2025-05-23 10:48:16 -0400

    Fix app admin for greenfield

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -18,7 +18,7 @@ data "aws_iam_openid_connect_provider" "github" {
 }
 
 data "aws_iam_role" "admin" {
-  name = var.app == "dpc" ? "ct-ado-bcda-application-admin" : "ct-ado-${var.app}-application-admin"
+  name = var.legacy && var.app == "ab2d" ? "ct-ado-ab2d-application-admin" : "ct-ado-bcda-application-admin"
 }
 
 data "aws_iam_role" "dasg_admin" {
