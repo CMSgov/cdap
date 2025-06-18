d4a2ea9eeccefb7929860a0c60084e9f8aa38413
Sean Fern <seanfern@navapbc.com>
2025-06-18 11:39:53 -0400

    Format bucket-access-logs/main.tf

--- terraform/services/bucket-access-logs/main.tf
+++ terraform/services/bucket-access-logs/main.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_access_log
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "aws:kms"
     }
     bucket_key_enabled = true
   }
