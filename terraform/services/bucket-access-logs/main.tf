c228fe24b339b1b1634946c27481ffd1ee86281a
Sean Fern <seanfern@navapbc.com>
2025-06-18 11:55:19 -0400

    Drop legacy refs in bucket-access-logs

--- terraform/services/bucket-access-logs/main.tf
+++ terraform/services/bucket-access-logs/main.tf
@@ -1,8 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "bucket_access_logs" {
-  bucket        = var.legacy == true ? "${data.aws_caller_identity.current.account_id}-bucket-access-logs" : null
-  bucket_prefix = var.legacy == false ? "bucket-access-logs-" : null
+  bucket_prefix = "bucket-access-logs-"
   force_destroy = true
 }
 
