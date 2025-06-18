7d486dd241fb7e56c81a66f22a4fac45d16b6e8a
Sean Fern <seanfern@navapbc.com>
2025-06-18 12:44:05 -0400

    PLT-1107 Enable bucket key on bucket-access-logs bucket (#254)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1107
    
    ## 🛠 Changes
    
    Enabled bucket key and clarified README. Dropped legacy references.
    
    ## ℹ️ Context
    
    Objects in the bucket-access-logs bucket that were encrypted with the
    old, missing key are inaccessible. All new items should be encrypted
    under the managed aws/s3 key.
    
    ## 🧪 Validation
    
    See checks.

--- terraform/services/bucket-access-logs/output.tf
+++ terraform/services/bucket-access-logs/output.tf
@@ -1,4 +1,4 @@
 output "name" {
   description = "The name of the S3 bucket used for bucket access logs"
-  value       = aws_s3_bucket.bucket_access_logs.bucket
+  value       = aws_s3_bucket.bucket_access_logs.id
 }
