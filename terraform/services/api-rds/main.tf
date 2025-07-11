4c9f0cc6cb897c2c0b0898e8e18324ce2b1d3eee
Sean Fern <seanfern@navapbc.com>
2025-07-11 13:20:10 -0400

    Drop db_access_from_mgmt sg rule

--- terraform/services/api-rds/main.tf
+++ terraform/services/api-rds/main.tf
@@ -70,16 +70,6 @@ resource "aws_vpc_security_group_ingress_rule" "db_access_from_controller" {
   security_group_id            = aws_security_group.sg_database.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "db_access_from_mgmt" {
-  count             = (var.app == "ab2d" || var.app == "dpc") ? 1 : 0
-  description       = "Management VPC Access"
-  from_port         = 5432
-  to_port           = 5432
-  ip_protocol       = "tcp"
-  cidr_ipv4         = data.aws_ssm_parameter.cdap_mgmt_vpc_cidr.value
-  security_group_id = aws_security_group.sg_database.id
-}
-
 resource "aws_vpc_security_group_ingress_rule" "quicksight" {
   count             = var.app != "ab2d" ? length(local.quicksight_cidr_blocks) : 0
   description       = "Allow inbound traffic from AWS QuickSight"
