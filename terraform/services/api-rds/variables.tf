7a0bd9175b2f79b21c09d9c5e993a06499fe0742
Sean Fern <seanfern@navapbc.com>
2025-07-14 11:49:19 -0400

    PLT-1149 Cleanup on workflow updates (#261)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1149
    
    ## 🛠 Changes
    
    Cleaned up issues from merge of #258.
    
    ## ℹ️ Context
    
    The apply workflows failed on api-rds and github-actions-roles when #258
    was merged. This should fix that.
    
    ## 🧪 Validation
    
    See checks.

--- terraform/services/api-rds/variables.tf
+++ terraform/services/api-rds/variables.tf
@@ -18,18 +18,6 @@ variable "env" {
   }
 }
 
-variable "jenkins_security_group_id" {
-  description = "Stores the security group managing Jenkins Agent for AB2D including account number for AB2D Management"
-  type        = string
-  default     = null
-}
-
-variable "mgmt_vpc_cidr" {
-  description = "CIDR for the Management VPC"
-  type        = string
-  default     = null
-}
-
 variable "name" {
   description = "If more than one RDS instance is needed, this variable should be set"
   type        = string
