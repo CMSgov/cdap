36187215f7c5868d6738e6aab0e9a7fb85bc60c3
Maboh Christopher <chris.maboh@aquia.io>
2025-07-17 12:57:59 -0400

    modify source reference

--- terraform/services/kms-keys/main.tf
+++ terraform/services/kms-keys/main.tf
@@ -1,5 +1,5 @@
 module "standards" {
-  source      = "../../modules/standards"
+  source      = "github.com/CMSgov/cdap//terraform/modules/standards?ref=0bd3eeae6b03cc8883b7dbdee5f04deb33468260"
   app         = var.app
   env         = var.env
   root_module = "https://github.com/CMSgov/cdap/tree/main/terraform/services/kms-keys"
