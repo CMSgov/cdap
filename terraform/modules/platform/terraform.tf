57d157c7ed13fe0652891087e47a16ad5eaaf0a6
juliareynolds-nava <juliareynolds@navapbc.com>
2025-07-28 11:47:01 -0600

    revert

--- terraform/modules/platform/terraform.tf
+++ terraform/modules/platform/terraform.tf
@@ -1,14 +1,9 @@
 terraform {
-  backend "s3" {
-    key = "platform/terraform.tfstate"
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = "~>5"
+      configuration_aliases = [aws.secondary]
+    }
   }
 }
-
-provider "aws" {
-}
-
-provider "aws" {
-  alias  = "secondary"
-  region = "us-west-2"
-}
-
