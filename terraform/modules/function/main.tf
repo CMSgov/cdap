b533ec0da2e37137b474b8b7c73909a963a6cf4c
juliareynolds-nava <juliareynolds@navapbc.com>
2025-08-04 10:10:59 -0600

    drop ab2d from local repos variable.

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -1,9 +1,6 @@
 locals {
   provider_domain = "token.actions.githubusercontent.com"
   repos = {
-    ab2d = [
-      "repo:CMSgov/ab2d:*",
-    ]
     bcda = [
       "repo:CMSgov/bcda-app:*",
     ]
