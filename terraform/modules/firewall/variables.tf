1e0aa4a54c7e024aa3c9e468371474244b47ee10
Julian Scott <julianscott@navapbc.com>
2025-08-07 12:09:08 -0700

    PR feedback updates.

--- terraform/modules/firewall/variables.tf
+++ terraform/modules/firewall/variables.tf
@@ -7,15 +7,6 @@ variable "app" {
   }
 }
 
-variable "env" {
-  description = "The application environment (dev, test, sbx, sandbox, prod)"
-  type        = string
-  validation {
-    condition     = contains(["dev", "test", "sbx", "sandbox", "prod"], var.env)
-    error_message = "Valid value for env is dev, test, sbx, sandbox, or prod."
-  }
-}
-
 variable "scope" {
   description = "Firewall scope"
   type        = string
