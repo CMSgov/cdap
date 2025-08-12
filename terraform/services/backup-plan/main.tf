cfaa40f01d1bbd8f9e6cb28189143a1861f9390d
juliareynolds-nava <juliareynolds@navapbc.com>
2025-08-12 15:13:40 -0600

    Looping over apps to create vaults, backup plans, and resources to be backed up.

---
+++ terraform/services/backup-plan/main.tf
@@ -0,0 +1,5 @@
+locals {
+  apps = ["AB2D", "BCDA", "DPC"]
+}
+
+data "aws_caller_identity" "current" {}
--- terraform/services/backup-plan/primary.tf
+++ terraform/services/backup-plan/primary.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_iam_role" "primary_kms_admin_role" {
   name = "KMSAdminRole"
 
@@ -109,11 +107,13 @@ data "aws_kms_key" "primary_kms_key" {
 }
 
 resource "aws_backup_vault" "primary_backup_vault" {
-  name        = var.vault_name
+  for_each = toset(local.apps)
+  name        = "${var.vault_name}_${each.value}"
   kms_key_arn = data.aws_kms_key.primary_kms_key.arn
 }
 
 data "aws_iam_policy_document" "primary_backup_policy" {
+  for_each = toset(local.apps)
   statement {
     effect = "Allow"
 
@@ -126,21 +126,23 @@ data "aws_iam_policy_document" "primary_backup_policy" {
       "backup:CopyIntoBackupVault",
     ]
 
-    resources = [aws_backup_vault.primary_backup_vault.arn]
+    resources = [aws_backup_vault.primary_backup_vault[each.value].arn]
   }
 }
 
 resource "aws_backup_vault_policy" "primary_backup_vault_policy" {
-  backup_vault_name = aws_backup_vault.primary_backup_vault.name
-  policy            = data.aws_iam_policy_document.primary_backup_policy.json
+  for_each = toset(local.apps)
+  backup_vault_name = aws_backup_vault.primary_backup_vault[each.value].name
+  policy            = data.aws_iam_policy_document.primary_backup_policy[each.value].json
 }
 
 resource "aws_backup_plan" "aws_backup_plan" {
-  name = "cdap_managed_backup_plan"
+  for_each = toset(local.apps)
+  name = "cdap_managed_backup_plan_${each.value}"
   #only the 4hr rule should be copied to secondary
   rule {
     rule_name         = "4Hourly_1"
-    target_vault_name = aws_backup_vault.primary_backup_vault.name
+    target_vault_name = aws_backup_vault.primary_backup_vault[each.value].name
     schedule          = "cron(0 */4 * * ? *)"
     start_window      = 60
     completion_window = 180
@@ -155,7 +157,7 @@ resource "aws_backup_plan" "aws_backup_plan" {
 
   rule {
     rule_name         = "Daily_7"
-    target_vault_name = aws_backup_vault.primary_backup_vault.name
+    target_vault_name = aws_backup_vault.primary_backup_vault[each.value].name
     schedule          = "cron(0 4 * * ? *)"
     start_window      = 60
     completion_window = 180
@@ -167,7 +169,7 @@ resource "aws_backup_plan" "aws_backup_plan" {
 
   rule {
     rule_name         = "Weekly_35"
-    target_vault_name = aws_backup_vault.primary_backup_vault.name
+    target_vault_name = aws_backup_vault.primary_backup_vault[each.value].name
     schedule          = "cron(0 0 ? * SAT *)"
     start_window      = 60
     completion_window = 180
@@ -179,7 +181,7 @@ resource "aws_backup_plan" "aws_backup_plan" {
 
   rule {
     rule_name         = "Monthly_90"
-    target_vault_name = aws_backup_vault.primary_backup_vault.name
+    target_vault_name = aws_backup_vault.primary_backup_vault[each.value].name
     schedule          = "cron(0 0 1 * ? *)"
     start_window      = 60
     completion_window = 180
@@ -200,10 +202,11 @@ resource "aws_backup_plan" "aws_backup_plan" {
 }
 
 resource "aws_backup_selection" "aws_backup_selection" {
+  for_each = toset(local.apps)
   # This iam role is the one CMS is using for their backup plan.
   iam_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/delegatedadmin/developer/cms-oit-aws-backup-service-role"
-  name         = "cdap_managed_backup_selection"
-  plan_id      = aws_backup_plan.aws_backup_plan.id
+  name         = "cdap_managed_backup_selection_${each.value}"
+  plan_id      = aws_backup_plan.aws_backup_plan[each.value].id
   # This database is the only entry for now until testing is complete.
-  resources    = ["arn:aws:rds:us-east-1:539247469933:cluster:dpc-${var.env}"]
+  resources    = [lower("arn:aws:rds:us-east-1:539247469933:cluster:${each.value}-${var.env}")]
 }
