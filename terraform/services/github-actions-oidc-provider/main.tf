2e8cb1765924a07bbe1e7fffd13f3cd67862f239
Maboh Christopher <chris.maboh@aquia.io>
2025-08-14 14:59:14 -0400

    fix  .terraform/modules/iam_github_oidc_provider/modules/iam-github-oidc-provider │ does not exist

--- terraform/services/github-actions-oidc-provider/main.tf
+++ terraform/services/github-actions-oidc-provider/main.tf
@@ -7,7 +7,6 @@ terraform {
 module "iam_github_oidc_provider" {
   source = "terraform-aws-modules/iam/aws//modules/iam-oidc-provider"
 
-
   tags = {
     business  = "oeda"
     code      = "https://github.com/CMSgov/cdap/tree/main/terraform/services/github-actions-oidc-provider"
