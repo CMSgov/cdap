59e0e191284b4fe0428e068a3702191f5961156d
juliareynolds-nava <juliareynolds@navapbc.com>
2025-08-21 17:18:46 -0600

    add dev env var

--- terraform/services/backup-plan/variables.tf
+++ terraform/services/backup-plan/variables.tf
@@ -8,7 +8,7 @@ variable "env" {
   description = "The application environment (test, prod)"
   type        = string
   validation {
-    condition     = contains(["test", "prod"], var.env)
-    error_message = "Valid value for env is test or prod."
+    condition     = contains(["dev","test","prod"], var.env)
+    error_message = "Valid value for env is dev, test, or prod."
   }
 }
