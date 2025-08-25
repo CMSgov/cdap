4783baa4f3c131babb108578c305b06d244099f1
juliareynolds-nava <juliareynolds@navapbc.com>
2025-08-25 12:17:30 -0600

    cost monitor and sns configuration

--- terraform/services/cost-anomaly/main.tf
+++ terraform/services/cost-anomaly/main.tf
@@ -1,43 +1,112 @@
-resource "aws_sns_topic" "cost_anomaly_alerts" {
-  name = "cost-anomaly-alerts-topic"
-}
+data "aws_caller_identity" "current" {}
 
-resource "aws_sns_topic_policy" "cost_anomaly_topic_policy" {
-  arn    = aws_sns_topic.cost_anomaly_alerts.arn
-  policy = data.aws_iam_policy_document.cost_anomaly_topic_access.json
-}
-
-data "aws_iam_policy_document" "cost_anomaly_topic_access" {
-  statement {
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["ce.amazonaws.com"]
-    }
-    actions   = ["sns:Publish"]
-    resources = [aws_sns_topic.cost_anomaly_alerts.arn]
-  }
-}
-
-resource "aws_ce_anomaly_monitor" "all_services_monitor" {
-  name              = "all-services-cost-monitor"
+resource "aws_ce_anomaly_monitor" "BCDA_Account_Monitor" {
+  name         = "BCDA Account Monitor"
   monitor_type      = "DIMENSIONAL"
   monitor_dimension = "SERVICE"
 }
 
-resource "aws_ce_anomaly_subscription" "cost_anomaly_subscription" {
-  name             = "cost-anomaly-alerts-subscription"
-  frequency        = "DAILY"
-  monitor_arn_list = [aws_ce_anomaly_monitor.all_services_monitor.arn]
+resource "aws_sns_topic" "cost_anomaly_updates" {
+  name = "cost-anomaly-updates"
+  kms_master_key_id = "alias/bcda-${var.env}"
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__cost_anomaly_sns_topic_policy"
+
+  statement {
+    sid = "AWSAnomalyDetectionSNSPublishingPermissions"
+
+    actions = [
+      "SNS:Publish"
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["costalerts.amazonaws.com"]
+    }
+
+    resources = [
+      aws_sns_topic.cost_anomaly_updates.arn
+    ]
+  }
+
+  statement {
+    sid = "__cost_anomaly_sns"
+
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        data.aws_caller_identity.current.account_id
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.cost_anomaly_updates.arn
+    ]
+  }
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.cost_anomaly_updates.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+resource "aws_ce_anomaly_subscription" "realtime_subscription" {
+  name      = "realtime_cost_anomaly_subscription"
+  frequency = "IMMEDIATE"
+
+  monitor_arn_list = [
+    aws_ce_anomaly_monitor.BCDA_Account_Monitor.arn
+  ]
+
   subscriber {
     type    = "SNS"
-    address = aws_sns_topic.cost_anomaly_alerts.arn
+    address = aws_sns_topic.cost_anomaly_updates.arn
   }
+
+  depends_on = [
+    aws_sns_topic_policy.default
+  ]
+
   threshold_expression {
-    dimension {
-      key           = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
-      match_options = ["GREATER_THAN_OR_EQUAL"]
-      values        = ["150"]
+    or {
+      dimension {
+        key           = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
+        match_options = ["GREATER_THAN_OR_EQUAL"]
+        values        = ["150"]
+      }
+    }
+    or {
+      dimension {
+        key           = "ANOMALY_TOTAL_IMPACT_PERCENTAGE"
+        match_options = ["GREATER_THAN_OR_EQUAL"]
+        values        = ["30"]
+      }
     }
   }
 }
--- terraform/services/cost-anomaly/variables.tf
+++ terraform/services/cost-anomaly/variables.tf
@@ -1,14 +1,5 @@
-variable "app" {
-  description = "The application name (ab2d, bcda, dpc, cdap)"
-  type        = string
-  validation {
-    condition     = contains(["ab2d", "bcda", "dpc", "cdap"], var.app)
-    error_message = "Valid value for app is ab2d, bcda, dpc, or cdap."
-  }
-}
-
 variable "env" {
-  description = "The application environment (dev, test, mgmt, sbx, sandbox, prod)"
+  description = "The application environment (dev, test, mgmt, sandbox, prod)"
   type        = string
   validation {
     condition     = contains(["dev", "test", "mgmt", "sandbox", "prod"], var.env)
