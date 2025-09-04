07d92abb3c523ae503254f2ed0ed483d5e1ab66c
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-04 15:17:20 -0600

    Initial checkin

---
+++ terraform/modules/ecs/main.tf
@@ -0,0 +1,24 @@
+module "platform" {
+  source      = "github.com/CMSgov/cdap//terraform/modules/platform"
+  providers   = { aws = aws, aws.secondary = aws.secondary }
+  app         = var.app
+  env         = var.env
+  root_module = "https://github.com/CMSgov/cdap/tree/main/terraform/modules/ecs"
+  service     = "fargate"
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.cluster_name}-${var.env}"
+
+  setting {
+    name  = "containerInsights"
+    value = module.platform.is_ephemeral_env ? "disabled" : "enabled"
+  }
+
+  configuration {
+    managed_storage_configuration {
+      fargate_ephemeral_storage_kms_key_id = var.cluster_kms_master_key_id
+      kms_key_id                           = var.cluster_kms_master_key_id
+    }
+  }
+}
