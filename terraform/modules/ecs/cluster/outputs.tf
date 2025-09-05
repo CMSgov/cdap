ccd744722b9c1ee4635e157f8107ec9598813fa2
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-05 14:26:04 -0600

    added clusterid

--- terraform/modules/ecs/cluster/outputs.tf
+++ terraform/modules/ecs/cluster/outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   description = "Name for the ecs cluster"
   value       = aws_ecs_cluster.ecs_cluster.name
 }
+
+output "cluster_id" {
+  description = "ID for the ecs cluster"
+  value       = aws_ecs_cluster.ecs_cluster.id
+}
