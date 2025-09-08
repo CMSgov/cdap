0a8800aeaceda104b2234e8d1875989ca13d10a7
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-08 09:58:25 -0600

    PR feedback

--- terraform/modules/ecs/cluster/main.tf
+++ terraform/modules/ecs/cluster/main.tf
@@ -1,4 +1,4 @@
-resource "aws_ecs_cluster" "ecs_cluster" {
+resource "aws_ecs_cluster" "this" {
   name = var.cluster_name
 
   setting {
--- terraform/modules/ecs/cluster/outputs.tf
+++ terraform/modules/ecs/cluster/outputs.tf
@@ -1,9 +1,4 @@
-output "name" {
-  description = "Name for the ecs cluster"
-  value       = aws_ecs_cluster.ecs_cluster.name
-}
-
-output "id" {
-  description = "ID for the ecs cluster"
-  value       = aws_ecs_cluster.ecs_cluster.id
+output "cluster" {
+  description = "The ecs cluster for the given inputs."
+  value       = aws_ecs_cluster.this
 }
