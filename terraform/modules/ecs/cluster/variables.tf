328ea2774a09f9c3953b58c002e89313f47cdde8
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-09 08:44:50 -0600

    pr feedback

--- terraform/modules/ecs/cluster/variables.tf
+++ terraform/modules/ecs/cluster/variables.tf
@@ -3,24 +3,6 @@ variable "platform" {
   type        = any
 }
 
-variable "app" {
-  description = "The application name (ab2d, bcda, dpc)"
-  type        = string
-  validation {
-    condition     = contains(["ab2d", "bcda", "dpc"], var.app)
-    error_message = "Valid value for app is ab2d, bcda, or dpc."
-  }
-}
-
-variable "env" {
-  description = "The application environment (dev, test, sandbox, prod)"
-  type        = string
-  validation {
-    condition     = contains(["dev", "test", "sandbox", "prod"], var.env)
-    error_message = "Valid value for env is dev, test,sandbox, or prod."
-  }
-}
-
 variable "cluster_name_override" {
   description = "Name of the ecs cluster."
   type        = string
