fc0960783ad3efed5cc614b3e6f0c43725240efe
Julia Reynolds <juliareynolds@navapbc.com>
2025-09-09 13:29:53 -0600

    PLT-1298 Create a terraform module for ecs/fargate clusters (#297)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1298
    
    ## 🛠 Changes
    
    added terraform for ecs cluster
    
    ## ℹ️ Context
    
    This work is to provide a standard template for Fargate resources.
    
    ## 🧪 Validation
    
    See plan
    
    ---------
    
    Co-authored-by: Sean Fern <seanfern@navapbc.com>
    Co-authored-by: Michael J Burling <michael.burling@cms.hhs.gov>

---
+++ terraform/modules/cluster/outputs.tf
@@ -0,0 +1,4 @@
+output "this" {
+  description = "The ecs cluster for the given inputs."
+  value       = aws_ecs_cluster.this
+}
---
+++ terraform/modules/cluster/variables.tf
@@ -0,0 +1,10 @@
+variable "platform" {
+  description = "Object that describes standardized platform values."
+  type        = any
+}
+
+variable "cluster_name_override" {
+  description = "Name of the ecs cluster."
+  type        = string
+  default     = null
+}
