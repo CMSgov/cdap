a1907bd5a67f081794022b8a1b7a55901c34d993
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-19 14:01:45 -0600

    Added lambda code

---
+++ terraform/services/cost-anomaly/lambda_src/lambda_function.py
@@ -0,0 +1,125 @@
+"""
+Receives messages from Cost Anomaly alarms via SQS subscription to SNS.
+Forwards the alarms to Slack, with an emoji that says good or bad.
+"""
+
+from datetime import datetime, timezone
+import json
+import os
+from urllib import request
+from urllib.error import URLError
+import boto3
+from botocore.exceptions import ClientError
+
+class Field:
+	def __init__(self,  type, text, emoji):
+		#type: plain_text
+		self.type = type
+		#text: text to be displayed
+		self.text = text
+		#emoji: boolean
+		self.emoji = emoji
+
+class Block:
+	#def __init__(self, type,  text=None, fields=None):
+	def __init__(self, type, **kwargs):
+		#type: section
+		self.type = type
+		#fields: an array of fields in the section
+		if kwargs.get("fields"):
+			self.fields = kwargs.get("fields")
+		if kwargs.get("text"):
+			self.text = kwargs.get("text")
+
+class Text:
+	#def __init__(self, type, text, emoji):
+	def __init__(self, type, text, **kwargs):
+		#type: plain_text
+		self.type = type
+		#text: text to be displayed
+		self.text = text
+		#emoji: boolean
+		if kwargs.get("emoji"):
+			self.emoji = kwargs.get("emoji")
+
+def is_ignore_ok():
+    """
+    Returns the current value of the IGNORE_OK environment variable.
+    This allows tests to patch the environment dynamically.
+    """
+    return os.environ.get('IGNORE_OK', 'false').lower() == 'true'
+
+def lambda_handler(event, context):
+
+    anomalyEvent = json.loads(event["Records"][0]["Sns"]["Message"])
+    message_id = anomalyEvent["messageId"]
+
+    #Total Cost of the Anomaly
+    totalcostImpact = anomalyEvent["impact"]["totalImpact"]
+
+    #Anomaly Detection Interval
+    anomalyStartDate =  anomalyEvent["anomalyStartDate"]
+    anomalyEndDate = anomalyEvent["anomalyEndDate"]
+
+    #anomalyDetailsLink
+    anomalyDetailsLink = anomalyEvent["anomalyDetailsLink"]
+
+    #Blocks is the main array that holds the full message.
+    blocks = []
+
+    headerText = Text("plain_text", ":warning: Cost Anomaly Detected ", emoji = True)
+    totalAnomalyCostText = Text("mrkdwn", "*Total Anomaly Cost*: $" + str(totalcostImpact))
+    rootCausesHeaderText = Text("mrkdwn", "*Root Causes* :mag:")
+    anomalyStartDateText = Text("mrkdwn", "*Anomaly Start Date*: " + str(anomalyStartDate))
+    anomalyEndDateText = Text("mrkdwn", "*Anomaly End Date*: " + str(anomalyEndDate))
+    anomalyDetailsLinkText = Text("mrkdwn", "*Anomaly Details Link*: " + str(anomalyDetailsLink))
+
+    blocks.append(Block("header", text=headerText.__dict__))
+    blocks.append(Block("section", text=totalAnomalyCostText.__dict__))
+    blocks.append(Block("section", text=anomalyStartDateText.__dict__))
+    blocks.append(Block("section", text=anomalyEndDateText.__dict__))
+    blocks.append(Block("section", text=anomalyDetailsLinkText.__dict__))
+    blocks.append(Block("section", text=rootCausesHeaderText.__dict__))
+
+    for rootCause in anomalyEvent["rootCauses"]:
+    	fields = []
+    	for rootCauseAttribute in rootCause:
+    	    if feature_flag_displayAccountName == True:
+    	        if rootCauseAttribute == "linkedAccount":
+    	            accountName = get_aws_account_name(rootCause[rootCauseAttribute])
+    	            fields.append(Field("plain_text", "accountName"  + " : " + accountName, False))
+    	    fields.append(Field("plain_text", rootCauseAttribute  + " : " + rootCause[rootCauseAttribute], False))
+    	blocks.append(Block("section", fields = [ob.__dict__ for ob in fields]))
+
+    message_json = blocks= json.dumps([ob.__dict__ for ob in blocks])
+
+    webhook = 'https://hooks.slack.com/services/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'
+
+    send_message_to_slack(webhook, message_json, message_id)
+
+def send_message_to_slack(webhook, message_json, message_id):
+    """
+    Calls the Slack webhook with the formatted message. Returns success status.
+    """
+    if not webhook:
+        log({'msg': 'Unable to send to Slack as webhook URL is not set',
+             'messageId': message_id})
+        return False
+    jsondata = message_json
+    jsondataasbytes = jsondata.encode('utf-8')
+    req = request.Request(webhook)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    req.add_header('Content-Length', str(len(jsondataasbytes)))
+    try:
+        with request.urlopen(req, jsondataasbytes) as resp:
+            if resp.status == 200:
+                log({'msg': 'Successfully sent message to Slack',
+                     'messageId': message_id})
+                return True
+            log({'msg': f'Unsuccessful attempt to send message to Slack ({resp.status})',
+                 'messageId': message_id})
+            return False
+    except URLError as e:
+        log({'msg': f'Unsuccessful attempt to send message to Slack ({e.reason})',
+             'messageId': message_id})
+        return False
--- terraform/services/cost-anomaly/main.tf
+++ terraform/services/cost-anomaly/main.tf
@@ -1,20 +1,20 @@
 data "aws_caller_identity" "current" {}
-
+locals {
+  function_name = "cost_anomaly_alert"
+}
 resource "aws_ce_anomaly_monitor" "BCDA_Account_Monitor" {
   name              = "BCDA Account Monitor"
   monitor_type      = "DIMENSIONAL"
   monitor_dimension = "SERVICE"
 }
 
-module "cost_anomaly_sns" {
-  source      = "../../modules/topic"
-  name        = "cost-anomaly-updates"
-  policy_service_identifiers = ["costalerts.amazonaws.com"]
+resource "aws_sns_topic" "cost_anomaly_sns" {
+  name              = "cost-anomaly-topic"
+  kms_master_key_id = "alias/bcda-${var.env}"
 }
 
-
 resource "aws_ce_anomaly_subscription" "realtime_subscription" {
-  name      = "realtime_cost_anomaly_subscription"
+  name      = "cost_anomaly_subscription"
   frequency = "IMMEDIATE"
 
   monitor_arn_list = [
@@ -23,7 +23,7 @@ resource "aws_ce_anomaly_subscription" "realtime_subscription" {
 
   subscriber {
     type    = "SNS"
-    address = module.cost_anomaly_sns.arn
+    address = aws_sns_topic.cost_anomaly_sns.arn
   }
 
   threshold_expression {
@@ -44,30 +44,33 @@ resource "aws_ce_anomaly_subscription" "realtime_subscription" {
   }
 }
 
-
-module "sns_to_slack_function" {
-  source = "../../modules/function"
-
-  app = "cdap"
-  env = var.env
-
-  name        = "Cost Anomaly Alert"
-  description = "Listens for Cost Anomaly Alerts and forwards to Slack"
-
-  handler = "lambda_function.lambda_handler"
-  runtime = "python3.13"
-
-  environment_variables = {
-
-    IGNORE_OK = true
-  }
-}
-
 module "sns_to_slack_queue" {
   source = "../../modules/queue"
 
   name          = "cost-anomaly-alert-queue"
-  sns_topic_arn = module.cost_anomaly_sns.arn
+  sns_topic_arn = aws_sns_topic.cost_anomaly_sns.arn
 
-  function_name = module.sns_to_slack_function.name
+  app           = "bcda"
+  env           = var.env
+  function_name = local.function_name
+}
+
+module "cost_anomaly_function" {
+  source = "../../modules/function"
+
+  app = "bcda"
+  env = var.env
+
+  name        = local.function_name
+  description = "Forwards cost anomaly alerts to Slack channel #dasg-metrics-and-insights."
+
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.13"
+
+  memory_size = 2048
+
+  environment_variables = {
+    ENV      = var.env
+    IGNORE_OK = true
+  }
 }
