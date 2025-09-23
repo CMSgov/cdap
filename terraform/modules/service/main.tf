174299e1cd8914c3b0d6f3329de50aa2e292e1d6
Julia Reynolds <juliareynolds@navapbc.com>
2025-09-23 12:27:48 -0600

    Update terraform/modules/service/main.tf
    Co-authored-by: Sean Fern <seanfern@navapbc.com>

--- terraform/modules/service/main.tf
+++ terraform/modules/service/main.tf
@@ -64,7 +64,7 @@ resource "aws_ecs_task_definition" "this" {
 }
 
 resource "aws_ecs_service" "this" {
-  name                 = local.service_name
+  name                 = "${var.platform.app}-${var.platform.env}-${local.service_name}"
   cluster              = var.cluster
   task_definition      = aws_ecs_task_definition.this.arn
   desired_count        = var.desired_count
