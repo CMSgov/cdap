a5c570bf685dc03ed8d99357b52477133a1ed9f0
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-26 08:41:31 -0600

    set working directory at top of workflow

--- terraform/modules/cluster/variables.tf
+++ terraform/modules/cluster/variables.tf
@@ -1,6 +1,14 @@
 variable "platform" {
   description = "Object that describes standardized platform values."
-  type        = any
+  type = object({
+    app = string,
+    env = string,
+    kms_alias_primary = object({
+      target_key_arn = string
+    }),
+    service          = string,
+    is_ephemeral_env = string
+  })
 }
 
 variable "cluster_name_override" {
