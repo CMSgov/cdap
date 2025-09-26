b89fb63d505d235396c21acb2c7a316bb7b5cbfa
Sean Fern <seanfern@navapbc.com>
2025-09-26 18:05:18 -0400

    Add parenthesis

--- terraform/modules/service/variables.tf
+++ terraform/modules/service/variables.tf
@@ -81,7 +81,7 @@ variable "platform" {
     env               = string
     kms_alias_primary = object({ target_key_arn = string })
     primary_region    = object({ name = string })
-    private_subnets   = map(object({ id = string })
+    private_subnets   = map(object({ id = string }))
     service           = string
   })
 }
