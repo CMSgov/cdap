4fcc9adcfa582d1dc91785cbfd3f0e8167794de6
Sean Fern <seanfern@navapbc.com>
2025-09-26 18:01:12 -0400

    Try map string for platform.private_subnets

--- terraform/modules/service/variables.tf
+++ terraform/modules/service/variables.tf
@@ -81,7 +81,7 @@ variable "platform" {
     env               = string
     kms_alias_primary = object({ target_key_arn = string })
     primary_region    = object({ name = string })
-    private_subnets   = object({ string = object })
+    private_subnets   = map(string)
     service           = string
   })
 }
