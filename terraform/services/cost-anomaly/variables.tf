a230bf7c4289cc8e4c229e8b8259a41484936ff6
juliareynolds-nava <juliareynolds@navapbc.com>
2025-09-30 09:37:59 -0600

    pr feedback

--- terraform/services/cost-anomaly/variables.tf
+++ terraform/services/cost-anomaly/variables.tf
@@ -2,9 +2,7 @@ variable "env" {
   description = "The application environment (dev, test, mgmt, sandbox, prod)"
   type        = string
   validation {
-    condition     = contains(["dev", "test", "mgmt", "sandbox", "prod"], var.env)
+    condition     = contains(["test", "prod"], var.env)
     error_message = "Valid value for env is dev, test, mgmt, sandbox, or prod."
   }
 }
-
-
