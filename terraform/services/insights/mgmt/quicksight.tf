b35e0db37e0fb821fb5e8189f5a61c91e63be0e2
Michael J Burling <michael.burling@cms.hhs.gov>
2025-09-30 15:22:29 -0500

    PLT-1224: Initial Management of DASG Insights

---
+++ terraform/services/insights/mgmt/quicksight.tf
@@ -0,0 +1,15 @@
+resource "aws_quicksight_account_settings" "this" {
+    aws_account_id                 = local.account_id
+    default_namespace              = "default"
+    termination_protection_enabled = true
+}
+
+resource "aws_quicksight_ip_restriction" "this" {
+  enabled = length(local.ip_restrictions) > 0
+
+  ip_restriction_rule_map = local.ip_restrictions
+
+  depends_on = [
+    module.sops
+  ]
+}
---
+++ terraform/services/insights/mgmt/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    key = "insights/mgmt/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = module.standards.default_tags
+  }
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = "us-west-2"
+  default_tags {
+    tags = module.standards.default_tags
+  }
+}
