2b23d9b3daeaf8a37fdaddb29a713f9d93710276
Michael J Burling <michael.burling@cms.hhs.gov>
2025-10-01 09:46:25 -0500

    Remove bucket_key module declaration, outputs

--- terraform/modules/bucket/main.tf
+++ terraform/modules/bucket/main.tf
@@ -1,8 +1,3 @@
-module "bucket_key" {
-  source      = "../key"
-  name        = "${var.name}-bucket"
-  description = "For ${var.name} S3 bucket and its access logs"
-  user_roles  = var.cross_account_read_roles
 }
 
 resource "aws_s3_bucket" "this" {
