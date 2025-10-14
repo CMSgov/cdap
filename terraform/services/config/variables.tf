9d3939e6b733fd8890190b2e77c089b68a488fab
juliareynolds-nava <juliareynolds@navapbc.com>
2025-10-14 08:05:45 -0600

    add mgmt env

--- terraform/services/config/variables.tf
+++ terraform/services/config/variables.tf
@@ -2,7 +2,7 @@ variable "env" {
   description = "The application environment (test, prod)"
   type        = string
   validation {
-    condition     = contains(["test", "prod"], var.env)
-    error_message = "Valid value for env is test or prod."
+    condition     = contains(["test", "prod", "mgmt"], var.env)
+    error_message = "Valid value for env is test, prod, or mgmt."
   }
 }
