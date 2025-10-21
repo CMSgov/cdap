fa20705380e3ed79f5da17eda40fd6a96555404c
juliareynolds-nava <juliareynolds@navapbc.com>
2025-10-21 16:08:14 -0600

    restore files

--- terraform/services/config/main.tf
+++ terraform/services/config/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 module "platform" {
-  source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=ff2ef539fb06f2c98f0e3ce0c8f922bdacb96d66"
+  source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=plt-1358_sops"
   providers = { aws = aws, aws.secondary = aws.secondary }
 
   app         = local.app
-  env         = local.env
+  env         = var.env
   root_module = "https://github.com/CMSgov/cdap/tree/terraform/services/config"
   service     = local.service
 }
@@ -24,8 +24,7 @@ locals {
 }
 
 module "sops" {
-  source = "github.com/CMSgov/cdap//terraform/modules/sops?ref=ff2ef539fb06f2c98f0e3ce0c8f922bdacb96d66"
-
+  source   = "github.com/CMSgov/cdap//terraform/modules/sops?ref=ff2ef539fb06f2c98f0e3ce0c8f922bdacb96d66"
   platform = module.platform
 }
 
