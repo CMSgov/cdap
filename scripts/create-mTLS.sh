c878a54d5ce16a0ada75a2dcf062689031dd1c5f
juliareynolds-nava <juliareynolds@navapbc.com>
2025-10-29 10:06:57 -0600

    correct days value

--- scripts/create-mTLS.sh
+++ scripts/create-mTLS.sh
@@ -2,14 +2,14 @@
 
 # Generate CA
 openssl req -x509 -newkey rsa:4096 -nodes \
-  -keyout ca-key-prod-ab2d.pem -out ca-cert-prod-ab2d.pem -days 3650 \
+  -keyout ca-key-prod-ab2d.pem -out ca-cert-prod-ab2d.pem -days 365 \
   -subj "/C=US/ST=Maryland/L=Baltimore/O=Centers for Medicare and Medicaid Services/CN=prod.ab2d.cms.gov" \
   -addext "basicConstraints=critical,CA:true" \
   -addext "keyUsage=critical,digitalSignature,cRLSign,keyCertSign"
 
 # Generate Server Certificate
 openssl req -newkey rsa:4096 -nodes \
-  -keyout server-key.pem -out server.csr \
+  -keyout prod-ab2d-server-key.pem -out server.csr \
   -subj "/C=US/ST=Virginia/L=Reston/O=CMS/CN=localhost"
 
 openssl x509 -req -days 365 \
