72cc9d59b5deee5cbaf2e2170b83c242395d250a
juliareynolds-nava <juliareynolds@navapbc.com>
2025-11-12 10:38:48 -0700

    providers and rename file

---
+++ terraform/services/cost-anomaly/tofu.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    key = "cost-anomaly/terraform.tfstate"
+  }
+}
+
+variable "region" {
+  default  = "us-east-1"
+  nullable = false
+  type     = string
+}
+
+variable "secondary_region" {
+  default  = "us-west-2"
+  nullable = false
+  type     = string
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+provider "aws" {
+  alias = "secondary"
+
+  region = var.secondary_region
+  default_tags {
+    tags = local.default_tags
+  }
+}
