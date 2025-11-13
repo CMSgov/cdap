79701ebef949d916b4083cf6b1e0bc15500cc59a
juliareynolds-nava <juliareynolds@navapbc.com>
2025-11-13 08:41:40 -0700

    pr feedback

--- terraform/services/config/tofu.tf
+++ terraform/services/config/tofu.tf
@@ -1,15 +1,3 @@
-variable "region" {
-  default  = "us-east-1"
-  nullable = false
-  type     = string
-}
-
-variable "secondary_region" {
-  default  = "us-west-2"
-  nullable = false
-  type     = string
-}
-
 provider "aws" {
   region = "us-east-1"
   default_tags {
@@ -19,7 +7,6 @@ provider "aws" {
 
 provider "aws" {
   alias = "secondary"
-
   region = "us-west-2"
   default_tags {
     tags = local.default_tags
