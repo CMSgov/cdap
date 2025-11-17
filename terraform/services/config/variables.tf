d086b157348247ee7a3fb4a6a083033e8f3e1dd9
juliareynolds-nava <juliareynolds@navapbc.com>
2025-11-17 14:22:10 -0700

    change test.sopsw.yaml to use the bcda test key

--- terraform/services/config/variables.tf
+++ terraform/services/config/variables.tf
@@ -2,7 +2,7 @@ variable "env" {
   description = "The application environment (test, prod, mgmt)"
   type        = string
   validation {
-    condition     = contains(["test", "prod", "mgmt"], var.env)
-    error_message = "Valid value for env is test, prod, or mgmt."
+    condition     = contains(["test", "prod"], var.env)
+    error_message = "Valid value for env is test or prod."
   }
 }
