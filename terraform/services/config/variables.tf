09ab3045761a7a2fa0158cba4a77ac80cc19069e
juliareynolds-nava <juliareynolds@navapbc.com>
2025-11-19 14:18:23 -0700

    pr feedback

--- terraform/services/config/variables.tf
+++ terraform/services/config/variables.tf
@@ -1,8 +1,8 @@
 variable "env" {
-  description = "The application environment (test, prod)"
+  description = "The application environment (test, prod, mgmt)"
   type        = string
   validation {
-    condition     = contains(["test", "prod"], var.env)
-    error_message = "Valid value for env is test or prod."
+    condition     = contains(["test", "prod", "mgmt"], var.env)
+    error_message = "Valid value for env is test, prod, or mgmt."
   }
 }
