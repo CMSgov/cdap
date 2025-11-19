0d4bf9a925efb85e76799ad1a8d35889f4cd7f36
juliareynolds-nava <juliareynolds@navapbc.com>
2025-11-19 14:33:42 -0700

    format fix

--- terraform/services/config/variables.tf
+++ terraform/services/config/variables.tf
@@ -2,7 +2,7 @@ variable "env" {
   description = "The application environment (test, prod)"
   type        = string
   validation {
-    condition     = contains(["test", "prod",], var.env)
+    condition     = contains(["test", "prod"], var.env)
     error_message = "Valid value for env is test or prod."
   }
 }
