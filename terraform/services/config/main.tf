dc0a912ad9749fab7dc0a93bc5a8a969a6c5989b
Julia Reynolds <juliareynolds@navapbc.com>
2025-11-20 10:18:55 -0700

    [PLT-1358] correct hash ref for platform module for SOPS config service (#342)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1358
    
    ## 🛠 Changes
    
    Corrected reference to the platform module to hash from branch
    reference.
    
    ## ℹ️ Context
    
    To correct merge errors.
    
    ## 🧪 Validation
    
    See plan in checks.

--- terraform/services/config/main.tf
+++ terraform/services/config/main.tf
@@ -1,6 +1,6 @@
 # TODO replace ref with hash after merging
 module "platform" {
-  source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=plt-1358_sops"
+  source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=9389a80e100ec6cbdf0e2fc25123678c9156ff73"
   providers = { aws = aws, aws.secondary = aws.secondary }
 
   app         = "bcda"
