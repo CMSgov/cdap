9389a80e100ec6cbdf0e2fc25123678c9156ff73
Julia Reynolds <juliareynolds@navapbc.com>
2025-11-20 09:44:08 -0700

    [PLT-1358] Add SOPS to cdap to manage SSM parameter store  (#324)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1358
    
    ## 🛠 Changes
    
    Added a config service that uses SOPS to store parameters
    
    ## ℹ️ Context
    
    Adoption of the SOPS standard for CDAP
    
    ## 🧪 Validation
    
    See successful test run here:
    https://github.com/CMSgov/cdap/actions/runs/19517296089
    
    ---------
    
    Co-authored-by: jscott-nava <julianscott@navapbc.com>
    Co-authored-by: Sean Fern <seanfern@navapbc.com>
    Co-authored-by: Michael Valdes <michaelvaldes@navapbc.com>

---
+++ terraform/services/config/main.tf
@@ -0,0 +1,25 @@
+# TODO replace ref with hash after merging
+module "platform" {
+  source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=plt-1358_sops"
+  providers = { aws = aws, aws.secondary = aws.secondary }
+
+  app         = "bcda"
+  env         = var.env
+  root_module = "https://github.com/CMSgov/cdap/tree/terraform/services/config"
+  service     = local.service
+}
+
+locals {
+  default_tags = module.platform.default_tags
+  env          = terraform.workspace
+  service      = "config"
+}
+
+module "sops" {
+  source   = "github.com/CMSgov/cdap//terraform/modules/sops?ref=ff2ef539fb06f2c98f0e3ce0c8f922bdacb96d66"
+  platform = module.platform
+}
+
+output "edit" {
+  value = module.sops.sopsw
+}
