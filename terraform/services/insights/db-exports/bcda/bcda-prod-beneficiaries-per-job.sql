bf04bf2835452e7e31c3f28c974f27efc74f9c38
Parwinder Bhagat <Parwinder.Bhagat@e14s.com>
2025-11-21 15:22:45 -0600

    adding job files for views

---
+++ terraform/services/insights/db-exports/bcda/bcda-prod-beneficiaries-per-job.sql
@@ -0,0 +1,20 @@
+SELECT cron.schedule_in_database(
+  's3_export_bcda_prod_beneficiaries_per_job',
+  '0 */6 * * *',
+  $$
+    SELECT *
+    FROM aws_s3.query_export_to_s3
+    (
+      'SELECT * FROM bcda_prod_beneficiaries_per_job',
+      aws_commons.create_s3_uri
+      (
+        'bcda-prod-aurora-export-2025xxxxxxxxxxxxxxxxxxxxxx',
+        'bcda-prod-beneficiaries-per-job.csv',
+        'us-east-1'
+      ),
+      options := 'format csv, HEADER true',
+      kms_key => '<aurora_export key arn>'
+    );
+  $$,
+  'main'
+);
---
+++ terraform/services/insights/db-exports/bcda/bcda-prod-unique-entities-making-v2-requests.sql
@@ -0,0 +1,20 @@
+SELECT cron.schedule_in_database(
+  's3_export_bcda_prod_unique_entities_making_v2_requests',
+  '0 */6 * * *',
+  $$
+    SELECT *
+    FROM aws_s3.query_export_to_s3
+    (
+      'SELECT * FROM bcda_prod_unique_entities_making_v2_requests',
+      aws_commons.create_s3_uri
+      (
+        'bcda-prod-aurora-export-2025xxxxxxxxxxxxxxxxxxxxxx',
+        'bcda-prod-unique-entities-making-v2-requests.csv',
+        'us-east-1'
+      ),
+      options := 'format csv, HEADER true',
+      kms_key => '<aurora_export key arn>'
+    );
+  $$,
+  'main'
+);
