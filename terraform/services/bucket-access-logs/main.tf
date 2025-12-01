a2250fbfd70d7d733c68d4cda2f2f729f87da4ca
juliareynolds-nava <juliareynolds@navapbc.com>
2025-12-01 13:47:53 -0700

    replace old parameter names with the new ones from sops

--- terraform/services/bucket-access-logs/main.tf
+++ terraform/services/bucket-access-logs/main.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 resource "aws_s3_bucket" "bucket_access_logs" {
   bucket_prefix = "bucket-access-logs-"
   force_destroy = true
@@ -103,7 +101,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_access_logs" {
 }
 
 resource "aws_ssm_parameter" "bucket_access_logs" {
-  name        = "/cdap/bucket-access-logs-bucket"
+  name        = " /cdap/sensitive/bucket-access-logs-bucket "
   value       = aws_s3_bucket.bucket_access_logs.id
   type        = "String"
   description = "S3 bucket for storing access logs from other buckets"
