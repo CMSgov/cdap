7c4bca478e8edeb6c257abe0aab26c2eeeedf39f
Sadibhatla <121067659+Sadibhatla@users.noreply.github.com>
2025-12-12 15:05:27 -0800

    Update prod-benes-searched.view.sql
    Added Contract name

--- terraform/services/insights/views/ab2d/prod-benes-searched.view.sql
+++ terraform/services/insights/views/ab2d/prod-benes-searched.view.sql
@@ -1,53 +1,30 @@
-CREATE VIEW ab2d_prod_benes_searched AS
-  SELECT 
-    contract_number,
-    job_uuid,
-    benes_searched,
-    TO_CHAR(created_at, 'yyyy-MM-ddThh:mm:ss') created_at, 
-    TO_CHAR(completed_at, 'yyyy-MM-ddThh:mm:ss') completed_at,
-    eobs_written,
-    time_to_complete,
-    data_start_time,
-    since,
-    fhir_version,
-    status,
-    contract_number AS "Contract Number", 
-    job_uuid AS "Job ID", 
-    benes_searched AS "# Bene Searched",
-    completed_at AS "Completed At",
-    eobs_written AS "# EoBs Written",
-    data_start_time AS "Data Start Date (Since Date)",
-    fhir_version AS "FHIR Version",
-    to_char(time_to_complete, 'HH24:MI:SS') AS "Seconds Run",
-    TO_CHAR(created_at, 'yyyy-MM-ddThh:mm:ss') "Job Start Time",
-    TO_CHAR(completed_at, 'yyyy-MM-ddThh:mm:ss') "Job Complete Time",
-    to_char(time_to_complete, 'HH24:MI:SS') AS sec_run,
-    TO_CHAR(created_at, 'yyyy-MM-ddThh:mm:ss') job_start_time,
-    TO_CHAR(completed_at, 'yyyy-MM-ddThh:mm:ss') job_complete_time
-  FROM (
-    SELECT 
-      s.contract_number, 
-      j.job_uuid, 
-      s.benes_searched, 
-      j.created_at, 
-      j.completed_at, 
-      s.eobs_written, 
-      j.completed_at - j.created_at as time_to_complete,
-      CASE
-        WHEN j.since is null
-        THEN
-          CASE
+select *, contract_number as "Contract Number", contract_name  as "Contract Name",
+job_uuid as "Job ID",
+benes_searched as "# Bene Searched",
+completed_at as "Completed At",
+eobs_written as "# EoBs Written",
+data_start_time as "Data Start Date (Since Date)",
+fhir_version as "FHIR Version",
+to_char(time_to_complete, 'HH24:MI:SS') as "Seconds Run",
+created_at as "Job Start Time",
+completed_at as "Job Complete Time",
+to_char(time_to_complete, 'HH24:MI:SS') as sec_run,
+created_at as job_start_time,
+completed_at as job_complete_time
+from (
+select s.contract_number, c.contract_name, j.job_uuid, s.benes_searched, j.created_at, j.completed_at, s.eobs_written,
+j.completed_at - j.created_at as time_to_complete,
+CASE
+    WHEN j.since is null THEN
+        CASE
             WHEN c.attested_on < '2020-01-01'
             THEN '2020-01-01'
             ELSE c.attested_on
-          END
-        ELSE j.since
-      END AS data_start_time,
-      j.since,
-      j.fhir_version,
-      j.status
-    FROM job j
-    LEFT JOIN event.event_bene_search s ON s.job_id = j.job_uuid
-    LEFT JOIN contract_view c ON c.contract_number = j.contract_number
-    WHERE j.started_by='PDP') t
-    ORDER BY "Job Start Time" DESC;
+        END
+    ELSE j.since
+end as data_start_time, j.since, j.fhir_version, j.status
+from job j
+left join event.event_bene_search s on s.job_id = j.job_uuid
+left join contract_view c on c.contract_number = j.contract_number and c.contract_name is not null
+where j.started_by='PDP') t
+order by "Job Start Time" desc
