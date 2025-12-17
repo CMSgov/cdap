9a645ee9943637e3c2cf1c5fa8ff8af3576f5dcd
Julian Scott <julianscott@navapbc.com>
2025-12-17 14:02:41 -0800

    Adding overrides for AB2D contracts.

--- terraform/modules/service/main.tf
+++ terraform/modules/service/main.tf
@@ -1,4 +1,6 @@
 locals {
+  awslogs_group     = var.awslogs_group_override != null ? var.awslogs_group_override : local.service_name
+  container_name    = var.container_name_override != null ? var.container_name_override : local.service_name
   service_name      = var.service_name_override != null ? var.service_name_override : var.platform.service
   service_name_full = "${var.platform.app}-${var.platform.env}-${local.service_name}"
 }
@@ -13,7 +15,7 @@ resource "aws_ecs_task_definition" "this" {
   memory                   = var.memory
   container_definitions = nonsensitive(jsonencode([
     {
-      name                   = local.service_name
+      name                   = local.container_name
       image                  = var.image
       readonlyRootFilesystem = true
       portMappings           = var.port_mappings
@@ -23,7 +25,7 @@ resource "aws_ecs_task_definition" "this" {
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "/aws/ecs/fargate/${var.platform.app}-${var.platform.env}/${local.service_name}"
+          awslogs-group         = "/aws/ecs/fargate/${var.platform.app}-${var.platform.env}/${local.awslogs_group}"
           awslogs-create-group  = "true"
           awslogs-region        = var.platform.primary_region.name
           awslogs-stream-prefix = "${var.platform.app}-${var.platform.env}"
