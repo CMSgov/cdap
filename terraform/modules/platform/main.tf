3485294a47d3a46d8f860fe265216d05f73e0d96
mianava <miaferguson@navapbc.com>
2025-12-22 14:38:09 -0500

    [PLT-1418] Update web module to support DPC, leverage STS headers and cloudfront logging; (#358)
    ## 🎫 Ticket
    
    jira.cms.gov/browse/PLT-1418
    
    ## 🛠 Changes
    
    This PR configures: 
    1) The 'web' module to configure an existing cloudfront deployment that
    supports STS headers, has a regional domain name
    ("domain_name_overwrite").
    2) The platform module to accommodate bucket logging in regional paths
    for Cloudfront logs passed into S3. This enables the passing of logs, by
    administrative AWS configuration, to an external provider.
    3) Simplifies some variables into strings instead of objects.
    
    ## ℹ️ Context
    
    These changes were made to support oversight and standardization of
    static site management through the centralization of terraform.
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->
    These changes require validation in the sandbox environment. The module
    source will be updated to point to this github commit hash as ref. Once
    the sandbox site is determined mostly unchanged, the module ref can be
    updated for the production site.

--- terraform/modules/platform/main.tf
+++ terraform/modules/platform/main.tf
@@ -114,6 +114,10 @@ data "aws_s3_bucket" "access_logs" {
   bucket = local.access_logs_bucket[local.parent_env]
 }
 
+data "aws_s3_bucket" "logs_to_splunk" {
+  bucket = "cms-cloud-${data.aws_caller_identity.this.account_id}-${data.aws_region.primary.name}"
+}
+
 data "aws_security_groups" "this" {
   filter {
     name   = "vpc-id"
