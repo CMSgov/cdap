d010cb72181bd56c8eb19306a417327d6d1abcd0
Julian Scott <julianscott@navapbc.com>
2026-01-09 10:34:13 -0800

    Setting default health check grace period to null.

--- terraform/modules/service/variables.tf
+++ terraform/modules/service/variables.tf
@@ -27,7 +27,7 @@ variable "cpu" {
 }
 
 variable "health_check_grace_period_seconds" {
-  default     = 300
+  default     = null
   description = "Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 2147483647. Only valid for services configured to use load balancers."
   type        = number
 }
