e6eb64bd8eeca22d36bac1e92b15ac4d90e94c7c
Michael J Burling <michael.burling@cms.hhs.gov>
2026-01-12 13:36:12 -0600

    Identify 'cdap' as the app at issue

--- terraform/services/config/main.tf
+++ terraform/services/config/main.tf
@@ -2,7 +2,7 @@ module "platform" {
   source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=9389a80e100ec6cbdf0e2fc25123678c9156ff73"
   providers = { aws = aws, aws.secondary = aws.secondary }
 
-  app         = "bcda"
+  app         = "cdap"
   env         = var.env
   root_module = "https://github.com/CMSgov/cdap/tree/terraform/services/config"
   service     = local.service
