b7fa93acff26a1f3581dcd98e91fb37e22d4cb76
Sean Fern <seanfern@navapbc.com>
2026-01-12 17:15:24 -0500

    PLT-1425 Update SOPS param keys to simplified convention (#361)
    ## 🎫 Ticket
    
    PLT-1425
    
    ## 🛠 Changes
    
    SOPS param keys updated in prod and test config values. Values files
    have been regenerated using the cdap-test and cdap-prod keys in both
    us-east-1 and us-west-2.
    
    ## ℹ️ Context
    
    This change sets shared SSM params via our SOPS config at conventional,
    consistent paths for all teams to use.
    
    The TARGET_ENVS for the config service have been updated to `cdap-test`
    and `cdap-prod`. Eventually, we should switch `TARGET_ENVS=account` to
    point at these two environments, but we are not ready to switch all
    account services to these new environments. Once that has been done,
    this service should be switched back to `TARGET_ENVS=account`.
    
    ## 🧪 Validation
    
    SOPS value files have been checked for correct values and can be
    decryped and encrypted with the specified key.
    
    ---------
    
    Co-authored-by: Michael J Burling <michael.burling@cms.hhs.gov>

--- terraform/services/config/main.tf
+++ terraform/services/config/main.tf
@@ -2,21 +2,22 @@ module "platform" {
   source    = "github.com/CMSgov/cdap//terraform/modules/platform?ref=9389a80e100ec6cbdf0e2fc25123678c9156ff73"
   providers = { aws = aws, aws.secondary = aws.secondary }
 
-  app         = "bcda"
+  app         = "cdap"
   env         = var.env
-  root_module = "https://github.com/CMSgov/cdap/tree/terraform/services/config"
+  root_module = "https://github.com/CMSgov/cdap/tree/main/terraform/services/config"
   service     = local.service
 }
 
 locals {
   default_tags = module.platform.default_tags
-  env          = terraform.workspace
   service      = "config"
 }
 
 module "sops" {
-  source   = "../../modules/sops"
-  platform = module.platform
+  source = "github.com/CMSgov/cdap//terraform/modules/sops?ref=8874310"
+
+  platform                  = module.platform
+  create_local_sops_wrapper = var.create_local_sops_wrapper
 }
 
 output "edit" {
