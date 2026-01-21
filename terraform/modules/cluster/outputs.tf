d929a9fa52a312d15711e75705bc6d3675898879
juliareynolds-nava <juliareynolds@navapbc.com>
2026-01-21 12:58:41 -0700

    added output for service connect discovery namespace

--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -2,3 +2,8 @@ output "this" {
   description = "The ecs cluster for the given inputs."
   value       = aws_ecs_cluster.this
 }
+
+output "service_connect_namespace" {
+  description = "The Service Connect discovery namespace."
+  value = aws_service_discovery_http_namespace.ecs-service-discovery
+}
