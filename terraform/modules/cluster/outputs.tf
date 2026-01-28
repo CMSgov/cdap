751da9c64206a11a36b62d9436a999aafe41762e
juliareynolds-nava <juliareynolds@navapbc.com>
2026-01-27 20:34:24 -0700

    remove ns for now

--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -2,8 +2,3 @@ output "this" {
   description = "The ecs cluster for the given inputs."
   value       = aws_ecs_cluster.this
 }
-
-output "service_connect_namespace" {
-  description = "The Service Connect discovery namespace."
-  value = aws_service_discovery_http_namespace.ecs-service-discovery
-}
