19baf4e41caedbc7aa4c5318414c7d4b7baa6154
mianava <miaferguson@navapbc.com>
2026-02-02 12:23:54 -0500

    Leverage service from platform module per pattern noted by @mjburling
    Co-authored-by: Michael J Burling <michael.burling@cms.hhs.gov>

--- terraform/modules/web/main.tf
+++ terraform/modules/web/main.tf
@@ -1,5 +1,5 @@
 locals {
-  naming_prefix = "${var.platform.app}-${var.platform.env}-${var.service}"
+  naming_prefix = "${var.platform.app}-${var.platform.env}-${var.platform.service}"
   caching_policy = {
     CachingDisabled  = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
     CachingOptimized = "658327ea-f89d-4fab-a63d-7e88639e58f6"
