5a9474d482c67b755b9218f6d421206227d7ae52
mianava <miaferguson@navapbc.com>
2026-02-03 00:56:01 -0500

    Assert cdap VPCs systematically.

--- terraform/modules/standards/main.tf
+++ terraform/modules/standards/main.tf
@@ -1,9 +1,11 @@
 locals {
-  app         = var.app
-  env         = var.env
-  root_module = var.root_module
-  service     = var.service
-  cdap_acct   = contains(["sandbox", "prod"], local.env) ? "prod" : "test"
+  app             = var.app
+  env             = var.env
+  root_module     = var.root_module
+  service         = var.service
+  cdap_vpc_env    = contains(["sandbox", "prod"], local.env) ? "prod" : "test"
+  cdap_vpc_region = replace(trimprefix(data.aws_region.current.name, "us-"), "/-[0-9]+$/", "")
+  cdap_vpc_name   = "cdap-${cdap_vpc_env}-${cdap_vpc_region}"
 
   static_tags = {
     application    = local.app
@@ -20,15 +22,20 @@ data "aws_region" "secondary" {
   provider = aws.secondary
 }
 
+data "aws_region" "current" {}
+trimmed_string = trimsuffix(trimprefix(local.original_string, "us-"), "-1")
 data "aws_caller_identity" "this" {}
 
 data "aws_iam_policy" "permissions_boundary" {
   name = "ct-ado-poweruser-permissions-boundary-policy"
 }
 
+locals {
+}
+
 data "aws_vpc" "mgmt_vpc" {
   filter {
     name   = "tag:Name"
-    values = ["cdap-east-${local.cdap_acct}"]
+    values = [local.cdap_vpc_name]
   }
 }
