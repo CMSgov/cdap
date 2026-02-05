787224b7527d796b7a7706b9b8412d02a065d945
jscott-nava <julianscott@navapbc.com>
2026-02-05 12:35:10 -0800

    PLT-1507 Adding additional bucket config support. (#384)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1507
    
    ## 🛠 Changes
    
    This PR adds an optional variable `additional_bucket_policy` and updates
    that support merging that policy with the existing default bucket
    policy.
    
    ## ℹ️ Context
    
    The bucket module currently includes a default bucket policy that
    restricts access to SSL traffic. The IDR DB Importer bucket requires
    additional bucket policy configuration however, so this PR is intended
    to allow for an additional bucket policy to be passed in to the module
    and merged with the existing default policy.
    
    ## 🧪 Validation
    
    <details>
    <summary>Tofu Apply Output (AB2D-TEST)</summary>
    
    ```
    OpenTofu will perform the following actions:
    
      # module.idr_db_importer_bucket.aws_s3_bucket_policy.this will be updated in-place
      ~ resource "aws_s3_bucket_policy" "this" {
            id     = "ab2d-test-idr-db-importer-202601xxxxxxxxxxxxxxxxxxxx"
          ~ policy = jsonencode(
              ~ {
                  ~ Statement = [
                        {
                            Action    = "s3:*"
                            Condition = {
                                Bool = {
                                    "aws:SecureTransport" = "false"
                                }
                            }
                            Effect    = "Deny"
                            Principal = {
                                AWS = "*"
                            }
                            Resource  = [
                                "arn:aws:s3:::ab2d-test-idr-db-importer-202601xxxxxxxxxxxxxxxxxxxx/*",
                                "arn:aws:s3:::ab2d-test-idr-db-importer-202601xxxxxxxxxxxxxxxxxxxx",
                            ]
                            Sid       = "AllowSSLRequestsOnly"
                        },
                      + {
                          + Action    = [
                              + "s3:ListBucket",
                              + "s3:GetObject",
                            ]
                          + Condition = {
                              + StringNotEquals = {
                                  + "aws:PrincipalArn" = [
                                      + "arn:aws:iam::xxxxxxxxxxxx:role/ab2d-test-idr-db-importer",
                                      + "arn:aws:iam::xxxxxxxxxxxx:role/delegatedadmin/developer/ab2d-test-idr-db-importer-task",
                                    ]
                                }
                            }
                          + Effect    = "Deny"
                          + Principal = {
                              + AWS = "*"
                            }
                          + Resource  = [
                              + "arn:aws:s3:::ab2d-test-idr-db-importer-202601xxxxxxxxxxxxxxxxxxxx/*",
                              + "arn:aws:s3:::ab2d-test-idr-db-importer-202601xxxxxxxxxxxxxxxxxxxx",
                            ]
                          + Sid       = "DenyReadAccess"
                        },
                    ]
                    # (1 unchanged attribute hidden)
                }
            )
            # (1 unchanged attribute hidden)
        }
    
    Plan: 0 to add, 1 to change, 0 to destroy.
    ```
    </details>
    
    ---------
    
    Co-authored-by: Sean Fern <seanfern@navapbc.com>

--- terraform/modules/bucket/variables.tf
+++ terraform/modules/bucket/variables.tf
@@ -1,3 +1,9 @@
+variable "additional_bucket_policies" {
+  default     = []
+  description = "A list of additional bucket policies to be merged with the default."
+  type        = list(string)
+}
+
 variable "app" {
   description = "The application name (ab2d, bcda, dpc, cdap)"
   type        = string
@@ -19,12 +25,6 @@ variable "env" {
 variable "name" {
   description = "Name for the S3 bucket"
   type        = string
-}
-
-variable "cross_account_read_roles" {
-  description = "Roles in other accounts that need read access to this S3 bucket"
-  type        = list(any)
-  default     = []
 }
 
 variable "ssm_parameter" {
