7963c54c3a5f6054e7fb0bb4daa10340bec59660
juliareynolds-nava <juliareynolds@navapbc.com>
2026-02-05 08:42:29 -0700

    data call for accountid

--- terraform/modules/service/data.tf
+++ terraform/modules/service/data.tf
@@ -1,7 +1,4 @@
-data "aws_ram_resource_share" "pace_ca" {
-  resource_owner = "OTHER-ACCOUNTS"
-  name           = "pace-ca-g1"
-}
+data "aws_caller_identity" "current" {}
 
 data "aws_acmpca_certificate_authority" "pace" {
   arn = one(data.aws_ram_resource_share.pace_ca.resource_arns)
@@ -9,4 +6,9 @@ data "aws_acmpca_certificate_authority" "pace" {
 
 data "aws_kms_alias" "kms_key" {
   name = "alias/cdap-${var.platform.env}"
+}
+
+data "aws_ram_resource_share" "pace_ca" {
+  resource_owner = "OTHER-ACCOUNTS"
+  name           = "pace-ca-g1"
 }
