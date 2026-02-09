2f21bef1de2d6fd1326e7106699250f610f4c66c
jscott-nava <julianscott@navapbc.com>
2026-02-09 13:59:34 -0800

    PLT-1592 Service updates to use latest function module, pinning versions. (#386)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/PLT-1592
    
    ## 🛠 Changes
    
    This PR includes the following updates:
    
    - The function module was updated to create and pass an IAM policy to
    the bucket module that configures test env bucket read access from the
    prod account.
    - Pins function module references for the following services to this
    branch* to prevent unintended updates going forward:
    
    ```
    admin-aco-deny
    admin-create-aco
    admin-create-aco-creds
    admin-create-group
    api-waf-sync
    cclf-import
    opt-out-export
    opt-out-import
    ```
    [* A followup PR will be required after this PR is merged to update the
    branch name refs to commit hash refs.]
    
    ## ℹ️ Context
    
    With the bucket module updates made in
    https://github.com/CMSgov/cdap/pull/384 a number of services began
    failing after unexpectedly picking up the changes due to the function
    module references not being set to a commit hash.
    
    ## 🧪 Validation
    
    The following run shows all jobs now passing with `No changes. Your
    infrastructure matches the configuration.` (except for an unrelated CDAP
    failure while trying to download the AWS provider):
    
    https://github.com/CMSgov/cdap/actions/runs/21764943535/job/62798198521

--- terraform/services/cclf-import/main.tf
+++ terraform/services/cclf-import/main.tf
@@ -27,7 +27,7 @@ data "aws_iam_policy_document" "assume_bucket_role" {
 }
 
 module "cclf_import_function" {
-  source = "../../modules/function"
+  source = "github.com/CMSgov/cdap//terraform/modules/function?ref=72d9e8fd2f8e8ac38dca1bcbda33842a49768277"
 
   app = var.app
   env = var.env
