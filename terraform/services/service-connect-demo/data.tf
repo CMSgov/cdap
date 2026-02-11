4bfceb9507ce54d582e2067442db236554c7b909
juliareynolds-nava <juliareynolds@navapbc.com>
2026-02-11 09:47:12 -0700

    refactored demo to test folder

--- terraform/services/service-connect-demo/data.tf
+++
@@ -1,8 +0,0 @@
-# data "aws_ram_resource_share" "pace_ca" {
-#   resource_owner = "OTHER-ACCOUNTS"
-#   name           = "pace-ca-g1"
-# }
-#
-# data "aws_acmpca_certificate_authority" "pace" {
-#   arn = one(data.aws_ram_resource_share.pace_ca.resource_arns)
-# }
