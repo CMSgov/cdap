f377eb434432543e39d7d1d5ab0f44bab2391c82
Parwinder Bhagat <Parwinder.Bhagat@e14s.com>
2026-02-20 12:39:40 -0600

    remove time limit and add creation date to runout

--- terraform/services/insights/views/bcda/prod-requests-for-runout-data.view.sql
+++ terraform/services/insights/views/bcda/prod-requests-for-runout-data.view.sql
@@ -1,9 +1,9 @@
--- Total number of requests made for prior year runout
+-- Runout requests made in PROD
 -- NO PHI/PII allowed!
 CREATE OR REPLACE VIEW bcda_prod_requests_for_runout_data AS
 select acos.cms_id,
-    jobs.request_url
+    jobs.request_url,
+    jobs.created_at
 from jobs
     join acos on acos.uuid = jobs.aco_id
-where jobs.request_url like '%/runout/%'
-    and jobs.created_at > DATE(NOW() - interval '1 year');
+where jobs.request_url like '%/runout/%';
