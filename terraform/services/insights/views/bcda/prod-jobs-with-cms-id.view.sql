3bafd02621290006c9284e06a17ec38e84bf2373
Parwinder Bhagat <contact@bhagat.me>
2026-03-03 13:15:34 -0600

    BCDA-9842: avoid PHI/PII future exposure by explicit col grab (#401)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/BCDA-9842
    
    ## 🛠 Changes
    
    Explicitly grab columns from the `jobs` table instead of all to avoid
    exposure of PHI/PII if additional columns get added in the future.
    
    ## 🧪 Validation
    
    Ran queries locally

--- terraform/services/insights/views/bcda/prod-jobs-with-cms-id.view.sql
+++ terraform/services/insights/views/bcda/prod-jobs-with-cms-id.view.sql
@@ -1,7 +1,14 @@
 -- Get all jobs with CMS ID added from acos table
 CREATE OR REPLACE VIEW bcda_prod_jobs_with_cms_id AS
 SELECT
-    jobs.*,
+    jobs.id,
+    jobs.aco_id,
+    jobs.request_url,
+    jobs.status,
+    jobs.created_at,
+    jobs.updated_at,
+    jobs.job_count,
+    jobs.transaction_time,
     acos.cms_id
 FROM jobs
 LEFT JOIN acos ON acos.uuid = jobs.aco_id;
