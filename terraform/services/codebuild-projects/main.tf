18e92ef2e47a80ec5ab960c8e77c7321eb9e0041
mianava <miaferguson@navapbc.com>
2026-03-15 23:51:40 -0400

    Open security group egress for build resources that do not come over tcp

--- terraform/services/codebuild-projects/main.tf
+++ terraform/services/codebuild-projects/main.tf
@@ -67,8 +67,8 @@ resource "aws_vpc_security_group_egress_rule" "codebuild_project" {
   security_group_id = aws_security_group.codebuild_project[each.key].id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = -1
   ip_protocol = "-1"
+  from_port   = -1
   to_port     = -1
 }
 
