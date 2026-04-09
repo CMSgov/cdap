3a6c42a9d6846237dfa003494d968c92e275f8dc
mianava <miaferguson@navapbc.com>
2026-04-09 13:53:40 -0400

    Use CMS managed PACE for TLS

--- terraform/modules/service/outputs.tf
+++ terraform/modules/service/outputs.tf
@@ -1,5 +1,5 @@
 output "service" {
-  description = "The ecs service for the given inputs."
+  description = "The ECS service resource."
   value       = aws_ecs_service.this
 }
 
@@ -14,7 +14,7 @@ output "ecs_service_id" {
 }
 
 output "task_definition" {
-  description = "The ecs task definition for the given inputs."
+  description = "The ECS task definition resource."
   value       = aws_ecs_task_definition.this
 }
 
@@ -26,4 +26,9 @@ output "target_group_arn" {
 output "listener_rule_arn" {
   description = "ARN of the ALB listener rule (if ALB integration is enabled)."
   value       = local.enable_alb_integration ? aws_lb_listener_rule.this[0].arn : null
+}
+
+output "service_connect_role_arn" {
+  description = "ARN of the Service Connect IAM role (if Service Connect is enabled)."
+  value       = var.enable_ecs_service_connect ? aws_iam_role.service_connect[0].arn : null
 }
