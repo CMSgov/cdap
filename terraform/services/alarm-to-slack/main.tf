4ed3b22d076f880235a86d5bd2376a9aad6e280f
Luke Short <lukeshortemail@gmail.com>
2026-04-16 12:13:01 -0700

    use "arm64" for all teams

--- terraform/services/alarm-to-slack/main.tf
+++ terraform/services/alarm-to-slack/main.tf
@@ -9,7 +9,7 @@ module "sns_to_slack_function" {
 
   app          = var.app
   env          = var.env
-  architecture = var.app == "arm64"
+  architecture = "arm64"
 
   name        = local.full_name
   description = "Listens for CloudWatch Alerts and forwards to Slack"
