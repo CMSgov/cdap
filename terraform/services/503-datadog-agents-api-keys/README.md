e21d8715b990d9524413a1716ad97890d6f8e9b9
mianava <miaferguson@navapbc.com>
2026-04-20 22:57:58 -0400

    Establish datadog api keys using CDAP managed datadog keys.

---
+++ terraform/services/503-datadog-agents-api-keys/README.md
@@ -0,0 +1,4 @@
+# The number of API keys in the Datadog organization are capped at 50, so they are all centrally managed by CDAP. 
+We anticipate the following API key spread: 
+per application team terraform repository: 
+- 1 API key per "prod" and "non-prod"
---
+++ terraform/services/503-datadog-agents-api-keys/tofu.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "~>4.4"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5"
+    }
+  }
+
+  backend "s3" {
+    key = "503-datadog-cicd-keys/terraform.tfstate"
+  }
+}
+
+provider "datadog" {
+  api_key = sensitive(data.aws_ssm_parameter.cdap_datadog_api_key.value)
+  app_key = sensitive(data.aws_ssm_parameter.cdap_datadog_application_key.value)
+  api_url = "https://api.ddog-gov.com"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = module.standards.default_tags
+  }
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = "us-west-2"
+  default_tags {
+    tags = module.standards.default_tags
+  }
+}
