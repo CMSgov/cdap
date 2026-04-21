fae8aaa9d4fc076113e7cf4cc7fb1edf259f119e
mianava <miaferguson@navapbc.com>
2026-04-21 16:50:13 -0400

    CDAP to manage teams

--- terraform/services/501-datadog-cicd-keys/main.tf
+++ terraform/services/501-datadog-cicd-keys/main.tf
@@ -1,9 +1,10 @@
 locals {
   default_permissions = {
-    api_key_manager   = false
-    dashboard_manager = true
-    monitors_manager  = true
-    users_manager     = false
+    api_key_manager    = false
+    dashboard_manager  = true
+    monitors_manager   = true
+    users_manager      = false
+    org_config_manager = false
   }
 
   resolved_permissions = {
@@ -35,10 +36,11 @@ module "datadog_application_key" {
   env    = var.env
 
   # permissions can be set per application via a map as needed, this current sets default permissions for all
-  api_key_manager   = local.resolved_permissions.api_key_manager
-  dashboard_manager = local.resolved_permissions.dashboard_manager
-  monitors_manager  = local.resolved_permissions.monitors_manager
-  users_manager     = local.resolved_permissions.users_manager
+  api_key_manager    = local.resolved_permissions.api_key_manager
+  dashboard_manager  = local.resolved_permissions.dashboard_manager
+  monitors_manager   = local.resolved_permissions.monitors_manager
+  users_manager      = local.resolved_permissions.users_manager
+  org_config_manager = local.resolved_permissions.org_config_manager
 }
 
 #--------------
@@ -51,12 +53,13 @@ module "datadog_api_key" {
   used_for = "cicd"
 }
 
-module "standards" {
-  source = "../../modules/standards"
+module "platform" {
+  source    = "../../modules/platform"
+  providers = { aws = aws, aws.secondary = aws.secondary }
 
-  app         = var.app
-  env         = var.env
-  root_module = "https://github.com/CMSgov/cdap/tree/main/terraform/services/datadog-agents-api-keys"
-  service     = "datadog"
-  providers   = { aws = aws, aws.secondary = aws.secondary }
+  app          = "cdap"
+  env          = "prod"
+  root_module  = "https://github.com/CMSgov/cdap/tree/main/terraform/services/${basename(abspath(path.module))}/"
+  service      = replace(basename(abspath(path.module)), "/^[0-9]+-/", "")
+  ssm_root_map = { init_datadog = "/dasgapi/sensitive/datadog/" }
 }
