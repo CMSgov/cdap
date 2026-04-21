23fd0ede6cebab8e44b177902f54b29e68be65da
mianava <miaferguson@navapbc.com>
2026-04-21 00:14:11 -0400

    Leverage cdap keys to establish config.

--- terraform/services/502-datadog-config/main.tf
+++ terraform/services/502-datadog-config/main.tf
@@ -6,3 +6,11 @@ resource "datadog_monitor_config_policy" "env_tag" {
     valid_tag_values = ["dev", "test", "stage", "sandbox", "prod"]
   }
 }
+
+data "aws_ssm_parameter" "cdap_datadog_api_key" {
+  name = "/cdap/prod/datadog/cicd/api_key"
+}
+
+data "aws_ssm_parameter" "cdap_datadog_application_key" {
+  name = "/cdap/prod/datadog/cicd/application_key"
+}
--- terraform/services/502-datadog-config/tofu.tf
+++ terraform/services/502-datadog-config/tofu.tf
@@ -10,3 +10,9 @@ terraform {
     key = "502-datadog-config/terraform.tfstate"
   }
 }
+
+provider "datadog" {
+  api_key = sensitive(data.aws_ssm_parameter.cdap_datadog_api_key.value)
+  app_key = sensitive(data.aws_ssm_parameter.cdap_datadog_application_key.value)
+  api_url = "https://api.ddog-gov.com"
+}
