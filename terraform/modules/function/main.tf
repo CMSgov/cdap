b85fc091a541c47c55b0de948c5b850acef6fe28
mianava <miaferguson@navapbc.com>
2026-04-27 10:18:00 -0400

    Use security group rule instead of inline so security gorup rules can be easily used elsewhere.

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -66,14 +66,17 @@ resource "aws_security_group" "function" {
   name        = "${var.name}-function"
   description = "For the ${var.name} function"
   vpc_id      = module.vpc.id
+}
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+# FixMe Refine egress
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = var.allowed_cidr_blocks ? var.allowed_cidr_blocks : [module.vpc.cidr_block]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.function.id
 }
 
 resource "aws_lambda_function" "this" {
