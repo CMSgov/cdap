745c66066901a22d6d288a975260e88e5d60aab7
Parwinder Bhagat <contact@bhagat.me>
2026-05-05 13:03:04 -0500

    BCDA-9967: add model short name (#461)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/BCDA-9967
    
    ## 🛠 Changes
    
    Adding `model_name` to the production and sandbox jobs view
    
    ## ℹ️ Context
    
    We want to have the Alternative Payment Model included in job request
    data so that it can be easily aggregated, without having to derive the
    model from each entity's CMS ID
    
    ## 🧪 Validation
    
    Executed queries against prod/sandbox locally

--- terraform/services/insights/views/bcda/prod-jobs-with-cms-id.view.sql
+++ terraform/services/insights/views/bcda/prod-jobs-with-cms-id.view.sql
@@ -11,6 +11,20 @@ SELECT
     jobs.transaction_time,
     jobs.benes_attributed_to_aco,
     acos.cms_id,
+    CASE
+        WHEN acos.cms_id ~ 'D\d{4}' THEN 'ACO REACH'
+        WHEN acos.cms_id ~ 'K\d{4}' THEN 'KCC'
+        WHEN acos.cms_id ~ 'C\d{4}' THEN 'KCC'
+        WHEN acos.cms_id ~ 'CT\d{6}' THEN 'MD TCoC'
+        WHEN acos.cms_id ~ '^A\d{4}' THEN 'SSP'
+        WHEN acos.cms_id ~ 'IOTA\d{3}' THEN 'IOTA'
+        WHEN acos.cms_id ~ 'GUIDE-\d{5}' THEN 'GUIDE'
+        WHEN acos.cms_id ~ 'DA\d{4}' THEN 'CDAC'
+        WHEN acos.cms_id ~ 'TEST\d{3}' THEN 'TEST'
+        WHEN acos.cms_id ~ 'V\d{3}' THEN 'NGACO'
+        WHEN acos.cms_id ~ 'E\d{4}' THEN 'CEC'
+        ELSE 'Unknown'
+    END AS model_name,
     sq.benes_with_data,
     sq.benes_retrieved_percent
 FROM jobs
