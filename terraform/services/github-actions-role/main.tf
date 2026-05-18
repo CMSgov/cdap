00d815e2f250bb5608d3db1d5f8caf51955811ef
mianava <miaferguson@navapbc.com>
2026-05-18 18:42:33 -0400

    Allow use of alias and function

--- terraform/services/github-actions-role/main.tf
+++ terraform/services/github-actions-role/main.tf
@@ -379,6 +379,7 @@ data "aws_iam_policy_document" "github_actions_policy" {
       "lambda:CreateEventSourceMapping",
       "lambda:CreateFunction",
       "lambda:GetAlias",
+      "lambda:GetFunction",
       "lambda:GetEventSourceMapping",
       "lambda:GetFunctionCodeSigningConfig",
       "lambda:GetPolicy",
