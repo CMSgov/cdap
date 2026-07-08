f422cd3d9eb3df53eaeed15e3ee91cf44f403193
Anna Montare <267455234+annamontare-nava@users.noreply.github.com>
2026-07-08 14:40:45 -0600

    use the require_full_window argument when creating the monitor resource

--- terraform/modules/datadog_monitors/custom.tf
+++ terraform/modules/datadog_monitors/custom.tf
@@ -14,6 +14,7 @@ resource "datadog_monitor" "custom" {
 
   notify_no_data    = var.monitor_config.shadow_mode ? false : each.value.notify_no_data
   no_data_timeframe = each.value.notify_no_data ? each.value.no_data_timeframe_minutes : null
+  require_full_window = each.value.require_full_window
 
   tags = concat(local.base_tags, each.value.tags)
 }
