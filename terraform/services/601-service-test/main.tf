7cabc2d5d6a2cd2d1b55b1aca2b98dc67d94a5a9
mianava <miaferguson@navapbc.com>
2026-07-13 11:28:10 -0400

    remove repo creation list

--- terraform/services/601-service-test/main.tf
+++ terraform/services/601-service-test/main.tf
@@ -2,15 +2,6 @@ locals {
   config = yamldecode(file("${path.module}/config/${var.env}.yml"))
 }
 
-# basic repo with versioning
-module "repo" {
-  for_each = toset(local.config.services)
-  source   = "../../modules/ecr_repo"
-
-  platform = module.platform
-  service  = each.value
-}
-
 # Advanced repo, tagged with release + temp tag classes in a single repo
 module "repo_release_pipeline" {
   source = "../../modules/ecr_repo"
