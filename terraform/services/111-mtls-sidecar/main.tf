b61ae856f09d4520e4680db0e03d601389e2ce22
mianava <miaferguson@navapbc.com>
2026-07-29 16:16:47 -0400

    Format

--- terraform/services/111-mtls-sidecar/main.tf
+++ terraform/services/111-mtls-sidecar/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "mtls_sidecar_image_tag" {
-  name   = "/cdap/${var.env}/nonsensitive/mtls-sidecar/image-tag"
-  type   = "String"
-  value  = "initial"
+  name  = "/cdap/${var.env}/nonsensitive/mtls-sidecar/image-tag"
+  type  = "String"
+  value = "initial"
 
   lifecycle {
     ignore_changes = [value]
