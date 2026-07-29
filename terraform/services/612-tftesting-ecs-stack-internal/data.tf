84f20a061bdef0940d100aa7de3cb20158c28e4f
mianava <miaferguson@navapbc.com>
2026-07-29 18:05:34 -0400

    Support config for ecs cluster info override

--- terraform/services/612-tftesting-ecs-stack-internal/data.tf
+++ terraform/services/612-tftesting-ecs-stack-internal/data.tf
@@ -1,5 +1,10 @@
+locals {
+  config       = yamldecode(file("${path.module}/config/${var.env}.yml"))
+  cluster_name = try(local.config.ecs.cluster, "cdap-${var.env}") # 👈 add this
+}
+
 data "aws_ecs_cluster" "cluster_test" {
-  cluster_name = "cdap-${var.env}-tftesting"
+  cluster_name = local.cluster_name
 }
 
 data "aws_route53_zone" "internal" {
