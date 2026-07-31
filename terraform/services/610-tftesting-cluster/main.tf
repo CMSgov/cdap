ff658b7edce6205f09bec377fb82d833847eb983
mianava <miaferguson@navapbc.com>
2026-07-31 18:59:16 -0400

    Add http namepsace to testing cluster

--- terraform/services/610-tftesting-cluster/main.tf
+++ terraform/services/610-tftesting-cluster/main.tf
@@ -2,4 +2,6 @@ module "cdap_cluster" {
   source                = "../../modules/cluster"
   platform              = module.platform
   cluster_name_override = "cdap-${var.env}-tftesting"
+  supports_service_connect = true
 }
+
