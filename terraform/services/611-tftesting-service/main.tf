8aeaaf4202393bf1d03e11d7105c2d7b84ab325c
mianava <miaferguson@navapbc.com>
2026-07-31 18:58:58 -0400

    Ensure testing passes healthcheck

--- terraform/services/611-tftesting-service/main.tf
+++ terraform/services/611-tftesting-service/main.tf
@@ -1,26 +1,27 @@
 locals {
   config        = yamldecode(file("${path.module}/config/${var.env}.yml"))
   desired_count = try(local.config.ecs.desired_count, 0)
-  cluster_name  = try(local.config.ecs.cluster, "cdap-${var.env}") # 👈 add this
+  cluster_name  = try(local.config.ecs.cluster, "cdap-${var.env}")
 }
 
-data "aws_ecs_cluster" "cluster_test" {
+data "aws_ecs_cluster" "tftesting" {
   cluster_name = local.cluster_name
 }
 
 module "tftesting_service" {
-  source               = "../../modules/service/"
-  desired_count        = local.desired_count
-  enable_datadog_agent = true
-  log_retention_days   = 30
+  enable_execute_command = true
+  source                 = "../../modules/service/"
+  desired_count          = local.desired_count
+  enable_datadog_agent   = true
+  log_retention_days     = 30
 
-  cluster_arn = data.aws_ecs_cluster.cluster_test.arn
+  cluster_arn = data.aws_ecs_cluster.tftesting.arn
   cpu         = 512
   memory      = 1024
 
 
   health_check = {
-    command     = ["CMD-SHELL", "pgrep -f ddtrace-run || exit 1"]
+    command     = ["CMD-SHELL", "curl -f http://localhost:8080/health || exit 1"]
     interval    = 30
     retries     = 3
     startPeriod = 30
