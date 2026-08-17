160cfc71d8e6dd469b2daa9c8f1e65246cdbc2c8
mianava <miaferguson@navapbc.com>
2026-08-17 13:01:12 -0400

    Add more s3 permissions

--- terraform/services/005-github-actions-role/policy_storage.tf
+++ terraform/services/005-github-actions-role/policy_storage.tf
@@ -82,7 +82,7 @@ data "aws_iam_policy_document" "github_actions_storage" {
       "s3:GetBucketAcl",
       "s3:GetBucketCORS",
       "s3:GetBucketLogging",
-      "s3:GetBucketNotification",
+      "s3:GetBucketNotification"
       "s3:GetBucketObjectLockConfiguration",
       "s3:GetBucketOwnershipControls",
       "s3:GetBucketPolicy",
@@ -95,6 +95,8 @@ data "aws_iam_policy_document" "github_actions_storage" {
       "s3:GetReplicationConfiguration",
       "s3:GetAccelerateConfiguration",
       "s3:ListBucket",
+      "s3:ListBucketVersions",
+      "s3:ListBucketMultipartUploads"
     ]
     resources = ["*"]
   }
