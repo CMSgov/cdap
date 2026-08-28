791a5b6737efdeebdbe52c303c895b4f446a08b7
mianava <miaferguson@navapbc.com>
2026-08-28 10:27:42 -0400

    Lifecycles are always required

--- terraform/services/101-ecr-repos/main.tf
+++ terraform/services/101-ecr-repos/main.tf
@@ -19,7 +19,6 @@ locals {
 
           tag_rules            = try(svc.tag_rules, [])
           untagged_expiry_days = try(svc.untagged_expiry_days, null)
-          lifecycle_enabled    = try(svc.lifecycle_enabled, true)
         }
       ]
     ]) : pair.key => pair
@@ -49,5 +48,4 @@ module "ecr_repo" {
 
   tag_rules            = each.value.tag_rules
   untagged_expiry_days = each.value.untagged_expiry_days
-  lifecycle_enabled    = each.value.lifecycle_enabled
 }
