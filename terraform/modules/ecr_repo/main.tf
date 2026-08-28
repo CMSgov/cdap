23d412f20bb5ef7d27eee0b5dc49442bc2cf3c0e
mianava <miaferguson@navapbc.com>
2026-08-28 10:27:42 -0400

    Lifecycles are always required

--- terraform/modules/ecr_repo/main.tf
+++ terraform/modules/ecr_repo/main.tf
@@ -22,7 +22,6 @@ resource "aws_ecr_repository" "this" {
 }
 
 resource "aws_ecr_lifecycle_policy" "this" {
-  count      = var.lifecycle_enabled ? 1 : 0
   repository = aws_ecr_repository.this.name
 
   policy = jsonencode({
