cff9b0f79c9ae17b362cad4e317e28cfa7c96045
mianava <miaferguson@navapbc.com>
2026-08-31 11:10:16 -0400

    PLT-1767 ECR Cleanup Looks up ECR Repos (#589)
    ## 🎫 Ticket
    
    PLT-1767
    
    ## 🛠 Changes
    
    <!-- What was added, updated, or removed in this PR? -->
    - Moves permissions to ECR cleanup 
    - Sets up function module to use service as name label per common
    patterns
    - Updates ECR repositories that are evaluated during dryrun
    - Adds common files, including Makefile, to the ECR clean up logic for
    stable development process
    - Adds sonarqube properites to support ECR clean up testing 
    
    ## ℹ️ Context
    The ECR Cleanup function was not able to be updated with the existing
    permissions for the GHA role. Switch so that the ECR cleanup does the
    lookup of the ECR repositories dynamically.
    
    <!-- Why were these changes made? Add background context suitable for a
    non-technical audience. -->
    
    <!-- If any of the following security implications apply, this PR must
    not be merged without Stephen Walter's approval. Explain in this section
    and add @SJWalter11 as a reviewer.
      - Adds a new software dependency or dependencies.
      - Modifies or invalidates one or more of our security controls.
      - Stores or transmits data that was not stored or transmitted before.
    - Requires additional review of security implications for other reasons.
    -->
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->
    - Validated by running local tests, including dry -run
    - Upon initial review, validated by provisioning to cdap-test and
    checking function deploy runs smoothly

--- terraform/services/900-github-actions-role/policy_cdap_only.tf
+++ terraform/services/900-github-actions-role/policy_cdap_only.tf
@@ -136,8 +136,36 @@ data "aws_iam_policy_document" "github_actions_cdap" {
   statement {
     sid = "S3SharedBucketTagging"
     actions = [
+      "s3:GetAccelerateConfiguration",
+      "s3:GetBucketAcl",
+      "s3:GetBucketCORS",
+      "s3:GetBucketLocation",
+      "s3:GetBucketLogging",
+      "s3:GetBucketNotification",
+      "s3:GetBucketOwnershipControls",
+      "s3:GetBucketPolicy",
+      "s3:GetBucketRequestPayment",
       "s3:GetBucketTagging",
+      "s3:GetBucketVersioning",
+      "s3:GetBucketWebsite",
+      "s3:GetEncryptionConfiguration",
+      "s3:GetLifecycleConfiguration",
+      "s3:GetBucketObjectLockConfiguration",
+      "s3:GetReplicationConfiguration",
+      "s3:ListBucket",
+      "s3:ListBucketVersions",
+      "s3:ListBucketMultipartUploads",
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:DeleteBucketPolicy",
+      "s3:PutBucketLogging",
+      "s3:PutBucketNotification",
+      "s3:PutBucketOwnershipControls",
+      "s3:PutBucketPolicy",
       "s3:PutBucketTagging",
+      "s3:PutBucketVersioning",
+      "s3:PutEncryptionConfiguration",
+      "s3:PutLifecycleConfiguration",
     ]
     resources = [
       "arn:aws:s3:::bucket-access-logs-*"
