27a3c2d1f85bf46ac9ed3faf489090476d9f80c8
Anna Smirnova <132938234+smirnovaae@users.noreply.github.com>
2026-09-01 12:46:04 -0700

    AB2D-7390 Added ab2d to variable "apps_served" (#602)
    ## 🎫 Ticket
    
    https://jira.cms.gov/browse/AB2D-7390
    
    ## 🛠 Changes
    
    Adds ab2d to the apps_served default so the alarm-to-slack lambda can
    serve AB2D alarms.
    
    ## ℹ️ Context
    
    AB2D team needs to have alerts from ECS
    - cdap-prod-alarm-to-slack has APPS=bcda,dpc,cdap and ab2d absent
    
    
    ## 🧪 Validation
    
    <!-- How were the changes verified? Did you fully test the acceptance
    criteria in the ticket? Provide reproducible testing instructions and
    screenshots if applicable. -->

--- terraform/services/201-alarm-to-slack/variables.tf
+++ terraform/services/201-alarm-to-slack/variables.tf
@@ -19,5 +19,5 @@ variable "env" {
 variable "apps_served" {
   description = "List of app names whose Slack webhook URLs this function reads from SSM at runtime."
   type        = list(string)
-  default     = ["bcda", "dpc", "cdap"]
+  default     = ["ab2d", "bcda", "dpc", "cdap"]
 }
