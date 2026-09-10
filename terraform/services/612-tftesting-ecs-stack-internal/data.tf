4c5e65ddc2bae1a44063a2ef6e2f27ac104e95ba
mianava <miaferguson@navapbc.com>
2026-09-10 10:41:19 -0400

    Remove ecs internal stack as the sidecar is not tested here.

--- terraform/services/612-tftesting-ecs-stack-internal/data.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ecs_cluster" "cluster_test" {
-  cluster_name = local.cluster_name
-}
-
-data "aws_route53_zone" "internal" {
-  name         = "${module.platform.env}.${module.platform.app}.internal.cms.gov"
-  private_zone = true
-}
